b9bb3257a57964b09dc898a33ef1f6a0c0e1ed2d
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-04-14 23:59:50 +0200

    Read text from an external file

---
+++ strings.asm
@@ -0,0 +1,65 @@
+section .text
+
+; Parse a space-delimited unsigned 16-bit integer
+; Input:
+;   DS:SI - string
+; Output:
+;   AX    - parsed integer
+;   CX    - field length
+;   CF    - parsing error
+;   DS:SI - first cell after the field
+parse_uint16:
+  push bx                     ; save registers
+  push dx
+
+  xor  ax, ax                 ; sum
+  xor  bx, bx                 ; character
+  xor  cx, cx                 ; count
+
+.lpad:
+  mov  bl, [si]
+  cmp  bl, ' '
+  jne  .digit
+
+  inc  cx
+  inc  si
+  jmp .lpad
+
+.digit:
+  mov  bl, [si]
+  cmp  bl, '0'
+  jb   .below_digit
+  cmp  bl, '9'
+  ja   .error                 ; invalid character
+
+  mov  dx, 10                 ; multiplier
+  mul  dx
+  sub  bl, '0'
+  add  ax, bx                 ; AX = (AX * 10) mod 65536 + BL
+
+  inc  cx
+  inc  si
+  jmp .digit
+
+.below_digit:
+  cmp  bl, ' '
+  jne  .error                 ; invalid character
+
+  inc  cx
+  inc  si
+
+.rpad:
+  mov  bl, [si]
+  cmp  bl, ' '
+  jne  .end
+
+  inc  cx
+  inc  si
+  jmp .rpad
+
+.error:
+  stc                         ; set CF
+.end:
+  pop  dx                     ; restore registers
+  pop  bx
+  ret
--- vii.asm
+++ vii.asm
@@ -3,23 +3,24 @@ cpu 8086
 
 LOGOW equ 272                 ; logo width
 LOGOH equ 100                 ; logo height
+LINE  equ 80                  ; max line length
 
 %define milliseconds(ms) (ms * 10 / 549)
 
 section .text
 
 start:
+  ; Set CGA video mode
   mov  ax, CGA_MODE_HIMONO    ; 640x200x2
   call cga_set_video_mode
   push ax                     ; save previous mode on stack
 
-  mov  si, msg_bronies
-  mov  ah, 32
-  mov  al, 18
-  call cga_draw_text
-
-  mov  cx, milliseconds(500)
-  call sleep
+  ; Open slideshow file
+  mov  ax, 3D00h              ; OPEN EXISTING FILE -> read only
+  mov  dx, slides
+  int  21h
+  jc   .error
+  mov  [fslides], ax
 
   mov  si, bitmap
   mov  ah, (640 - LOGOW) / 2 / 8
@@ -28,56 +29,50 @@ start:
   mov  dx, LOGOH
   call cga_draw_bitmap
 
-  mov  cx, milliseconds(500)
-  call sleep
+.next:
+  call line_read
+  jc   .error
+  test ax, ax
+  jz   .eof
+  push si
+  
+  call line_execute
+  pop  si
+  jmp .next
 
-  mov  si, msg_version
-  mov  ah, 35
-  mov  al, 19
-  call cga_draw_text
+.eof:
+  ; Close file
+  mov  ah, 3Eh
+  int  21h
+  jmp  .end
 
-  mov  cx, milliseconds(500)
-  call sleep
-
-  mov  si, msg_copyright
-  mov  ah, 10
-  mov  al, 23
-  call cga_draw_text
-
-  mov  cx, milliseconds(500)
-  call sleep
-
-  mov  si, msg_press
-  mov  ah, 24
-  mov  al, 24
-  call cga_draw_text
-
+.end:
   xor  ah, ah                 ; GET KEYSTROKE
   int  16h
 
   pop  ax                     ; restore saved mode
   call cga_set_video_mode
-  
+
   mov  ax, 4C00h              ; TERMINATE WITH RETURN CODE -> 0
   int  21h
 
-; Halt execution for a given period
-; Input:
-;   CX - number of ticks (1 tick ~ 54,9 ms)
-; Invalidates: CX
-sleep:
-  hlt
-  loop sleep
-  ret
+.error:
+  push ax
+  mov  ah, 09h                ; WRITE STRING TO STANDARD OUTPUT
+  mov  dx, msg_err
+  int  21h
+
+  pop  ax
+  mov  ah, 4Ch                ; TERMINATE WITH RETURN CODE -> AL
+  int  21h
+
 
 %include "cga.asm"
+%include "lineexec.asm"
 
 section .data
   bitmap            incbin "cgihisym.pbm", 57
-  msg_bronies       db     "Bronies Twilight", 0
-  msg_version       db     "Wersja 2.0", 0
-  msg_copyright     db     "Copyright (c) Fundacja BT, 2021. Wszelkie prawa zastrzezone.", 0
-  msg_press         db     "Nacisnij ENTER, aby kontynuowac.", 0
+  slides            db     "slides.txt", 0
+  msg_err           db     "ERR$"
 
 section .bss
-  
