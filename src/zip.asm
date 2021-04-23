771f18ec2c3e915cd5e49c70b43b2f72070625cf
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-04-23 18:42:23 +0200

    ZIP: Allow for case-insensitive search
    - when the file comes from FAT or NTFS, look for InfoZip Unicode Path field
    - verify Local File Header signature before looking for data

--- src/zip.asm
+++ src/zip.asm
@@ -51,8 +51,6 @@ ZipLocateFileHeader:
                 je      .CheckName
                 ERR     ZIP_CDIR_INVALID
 .CheckName:
-                cmp     word [di + ZIP_CDIR_FILE_HEADER.NameLength], cx
-                jne     .Iterate
                 cmp     byte [di + ZIP_CDIR_FILE_HEADER.Version + 1], ZIP_VERSION_FS_MSDOS
                 je      .CaseInsensitive
                 cmp     byte [di + ZIP_CDIR_FILE_HEADER.Version + 1], ZIP_VERSION_FS_NTFS
@@ -60,21 +58,54 @@ ZipLocateFileHeader:
                 cmp     byte [di + ZIP_CDIR_FILE_HEADER.Version + 1], ZIP_VERSION_FS_VFAT
                 jne     .CaseSensitive
 .CaseInsensitive:
+                cmp     word [di + ZIP_CDIR_FILE_HEADER.NameLength], cx
+                jne     .CheckExtraFields
                 push    si
                 mov     si, di
                 add     si, ZIP_CDIR_FILE_HEADER.Name
                 call    UniCompareUtf8IgnoreCase
                 pop     si
-                jc      .Error
+                jc      .Iterate
                 jne     .Iterate
                 jmp     .LocateLocalHeader
 .CaseSensitive:
+                cmp     word [di + ZIP_CDIR_FILE_HEADER.NameLength], cx
+                jne     .Iterate
                 push    si
                 mov     si, di
                 add     si, ZIP_CDIR_FILE_HEADER.Name
                 call    StrCompareMemory
                 pop     si
                 je      .LocateLocalHeader
+.CheckExtraFields:
+                push    si
+                push    cx
+                mov     si, di
+                add     si, ZIP_CDIR_FILE_HEADER_size
+                add     si, word [di + ZIP_CDIR_FILE_HEADER.NameLength]
+                mov     cx, si
+                add     cx, word [di + ZIP_CDIR_FILE_HEADER.ExtraLength]
+.NextExtraField:
+                cmp     si, cx
+                jae     .NotInExtraFields
+                cmp     word [si + ZIP_EXTRA_FIELD_HEADER.Signature], ZIP_EXTRA_INFOZIP_UNICODE_PATH
+                je      .InfozipUnicodePath
+                add     si, word [si + ZIP_EXTRA_FIELD_HEADER.TotalSize]
+                add     si, ZIP_EXTRA_FIELD_HEADER_size
+                jmp     .NextExtraField
+.InfozipUnicodePath:
+                mov     cx, word [si + ZIP_EXTRA_FIELD_HEADER.TotalSize]
+                sub     cx, (ZIP_EXTRA_INFOZIP_UNICODE_PATH_FIELD.UnicodeName - ZIP_EXTRA_FIELD_HEADER_size)
+                add     si, ZIP_EXTRA_INFOZIP_UNICODE_PATH_FIELD.UnicodeName
+                call    UniCompareUtf8IgnoreCase
+                jc      .NotInExtraFields
+                jne     .NotInExtraFields
+                pop     cx
+                pop     si
+                jmp     .LocateLocalHeader
+.NotInExtraFields:
+                pop     cx
+                pop     si
 .Iterate:
                 push    cx
                 mov     cx, ZIP_CDIR_FILE_HEADER_size
@@ -109,6 +140,14 @@ ZipLocateFileHeader:
 
                 global  ZipLocateFileData
 ZipLocateFileData:
+                cmp     word [di + ZIP_LOCAL_FILE_HEADER.PkSignature], ZIP_PK_SIGN
+                je      .CheckLocalSignature
+                ERR     ZIP_LOCAL_INVALID
+.CheckLocalSignature:
+                cmp     word [di + ZIP_LOCAL_FILE_HEADER.HeaderSignature], ZIP_LOCAL_FILE_SIGN
+                je      .CheckMethod
+                ERR     ZIP_LOCAL_INVALID
+.CheckMethod:
                 cmp     word [di + ZIP_LOCAL_FILE_HEADER.Compression], ZIP_METHOD_STORE
                 je      .CheckFlags
                 ERR     ZIP_UNSUPPORTED_METHOD
