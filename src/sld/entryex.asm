d75718932629432b0dc72baaffd42a0f19e2883a
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-08-04 01:32:00 +0200

    Slides: Allow for selecting bitmaps depending on the pixel aspect ratio

--- src/sld/entryex.asm
+++ src/sld/entryex.asm
@@ -74,7 +74,7 @@ SldEntryExecuteBitmap:
                 add     bx, SLD_ENTRY.szContent
                 xor     ch, ch
                 mov     cl, byte [di + SLD_ENTRY.bLength]
-                call    KerSearchArchive
+                call    SldLocateBestBitmap
                 jc      .Error
                 call    KerGetArchiveData               ; file data in DS:BX
                 jc      .Error
@@ -116,6 +116,112 @@ SldEntryExecuteBitmap:
                 pop     cx
                 pop     bx
                 pop     ax
+                ret
+
+
+; Locate the most fitting bitmap for a given file name
+; Input:
+;   DS:SI - ZIP central directory end structure
+;   DS:BX - file name
+;   CX    - file name length
+; Output:
+;   DS:DI - ZIP local file header structure
+;   CF    - error
+; Invalidates: BX, CX
+SldLocateBestBitmap:
+                push    ax              ; STACK COMPOSITION, relative to BP
+                push    bp              ;  +2  - preserved AX
+                mov     bp, sp          ;  -0  - preserved BP
+                sub     sp, 4           ;  -2  - file name begin pointer
+                                        ;  -4  - field begin pointer
+                mov     word [bp - 2], bx
+                mov     ax, bx
+                add     ax, cx
+                dec     ax
+.FindPattern:
+                cmp     byte [bx], '<'
+                je      .CheckPattern
+                inc     bx
+                cmp     bx, ax
+                je      .NoPattern
+                jmp     .FindPattern
+.CheckPattern:
+                cmp     byte [bx + 1], '>'
+                jne     .FindPattern
+                mov     word [bp - 4], bx
+
+                ; Pixel Aspect Ratio field
+                xor     ax, ax
+                call    VidGetPixelAspectRatio          ; AX - reference PAR
+                mov     bx, word [bp - 2]               ; BX - file name
+                mov     dx, word [bp - 4]               ; CX - file name length
+                call    SldFindBitmapNearestPar         ; DX - pixel aspect ration file name field
+                jc      .End
+.NoPattern:
+                mov     bx, word [bp - 2]
+                call    KerSearchArchive
+.End:
+                mov     sp, bp
+                pop     bp
+                pop     ax
+                ret
+
+
+; Locate the bitmap with the nearest PAR
+; Input:
+;   DS:SI - ZIP central directory end structure
+;   AX    - reference PAR
+;   DS:BX - file name
+;   CX    - file name length
+;   DS:DX - pixel aspect ration file name field
+; Output:
+;   DS:DI - ZIP local file header structure
+;   CF    - error
+; Invalidates: AX, BX, CX
+SldFindBitmapNearestPar:
+                push    bp              ; STACK COMPOSITION, relative to BP
+                mov     bp, sp          ;  -0  - preserved BP
+                sub     sp, 8           ;  -2  - current decreasing PAR
+                                        ;  -4  - current increasing PAR
+                                        ;  -6  - file name
+                                        ;  -8  - file name length
+                mov     word [bp - 2], ax
+                mov     word [bp - 4], ax
+                inc     word [bp - 2]
+                mov     word [bp - 6], bx
+                mov     word [bp - 8], cx
+.CheckDecreasing:
+                mov     ax, word [bp - 2]
+                cmp     ax, 0
+                jz      .CheckIncreasing
+                call    .Check
+                jnc     .End
+                dec     word [bp - 2]
+.CheckIncreasing:
+                mov     ax, word [bp - 4]
+                cmp     ax, 0FFh
+                je      .CheckEnd
+                call    .Check
+                jnc     .End
+                inc     word [bp - 4]
+.CheckEnd:
+                cmp     word [bp - 2], 0
+                jnz     .CheckDecreasing
+                stc
+.End:
+                mov     sp, bp
+                pop     bp
+                ret
+.Check:
+                call    KerByteToHex
+                xchg    al, ah
+                xchg    bx, dx
+                mov     word [bx], ax                   ; replace pattern with number
+                xchg    bx, dx
+
+                mov     bx, word [bp - 6]
+                mov     cx, word [bp - 8]
+                call    KerSearchArchive
                 ret
 
 
