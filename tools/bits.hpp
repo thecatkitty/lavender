7099da4299e3df43a93eadaae0c09ec5c70e6a15
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-08-03 12:23:40 +0200

    Tools: Add file XOR-ing utility

---
+++ tools/bits.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+
+
+inline constexpr uint64_t umaxval(unsigned bits) {
+    return (1Ui64 << bits) - 1;
+}
+
+template<typename T>
+inline constexpr T rotl(T n, unsigned c, unsigned l = sizeof(T) * CHAR_BIT)
+{
+     return (n << c | n >> (l - c)) & umaxval(l);
+}
+
+template<typename T>
+inline constexpr T rotr(T n, unsigned c, unsigned l = sizeof(T) * CHAR_BIT)
+{
+     return (n >> c | n << (l - c)) & umaxval(l);
+}
