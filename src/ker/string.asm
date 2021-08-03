7a7176ce0ee06bb43d07ee5f64f1decedf3fa656
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-08-04 01:30:18 +0200

    Kernel: Add a function for hexadecimal byte representation

--- src/ker/string.asm
+++ src/ker/string.asm
@@ -64,6 +64,33 @@ KerParseU16:
                 ret
 
 
+                global  KerByteToHex
+KerByteToHex:
+                push    bx
+                xor     bx, bx
+                mov     bl, al
+                and     bl, 0Fh
+
+                and     ax, 00F0h
+                shl     ax, 1
+                shl     ax, 1
+                shl     ax, 1
+                shl     ax, 1
+                or      ax, bx
+                add     ax, ('0' << 8) | '0'
+
+                cmp     al, '9'
+                jbe     .CheckHigh
+                add     al, 'A' - '9' - 1
+.CheckHigh:
+                cmp     ah, '9'
+                jbe     .End
+                add     ah, 'A' - '9' - 1
+.End:
+                pop     bx
+                ret
+
+
                 global  KerCompareMemory
 KerCompareMemory:
                 push    si
