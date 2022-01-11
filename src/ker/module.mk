d294c5d6297148027bb937fccbf6447dc3471602
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-11 21:49:59 +0100

    Kernel: Move text/number conversion helper routines out

--- src/ker/module.mk
+++ src/ker/module.mk
@@ -1,5 +1,5 @@
 export GIT_COMMIT = $(shell git rev-parse --short HEAD)
 export GIT_TAG    = $(shell git describe --tags $(GIT_COMMIT) --abbrev=0)
 
-ASSOURCES := $(ASSOURCES) $(DIR)/start.asm $(DIR)/string.asm $(DIR)/messages.asm
+ASSOURCES := $(ASSOURCES) $(DIR)/start.asm $(DIR)/messages.asm
 CCSOURCES := $(CCSOURCES) $(DIR)/error.c $(DIR)/info.c $(DIR)/int.c $(DIR)/time.c $(DIR)/unicode.c $(DIR)/zip.c
