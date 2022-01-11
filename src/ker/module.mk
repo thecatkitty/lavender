f82d44cac2bd82275dd9c1ca601581ad80e83a51
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-11 18:03:20 +0100

    Kernel: Move KerTerminate implementation to C

--- src/ker/module.mk
+++ src/ker/module.mk
@@ -1,5 +1,5 @@
 export GIT_COMMIT = $(shell git rev-parse --short HEAD)
 export GIT_TAG    = $(shell git describe --tags $(GIT_COMMIT) --abbrev=0)
 
-ASSOURCES := $(ASSOURCES) $(DIR)/start.asm $(DIR)/error.asm $(DIR)/string.asm $(DIR)/messages.asm
-CCSOURCES := $(CCSOURCES) $(DIR)/info.c $(DIR)/int.c $(DIR)/time.c $(DIR)/unicode.c $(DIR)/zip.c
+ASSOURCES := $(ASSOURCES) $(DIR)/start.asm $(DIR)/string.asm $(DIR)/messages.asm
+CCSOURCES := $(CCSOURCES) $(DIR)/error.c $(DIR)/info.c $(DIR)/int.c $(DIR)/time.c $(DIR)/unicode.c $(DIR)/zip.c
