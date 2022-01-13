2a5e33979c8c2df848438ab0238ae17b01c66e7d
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-13 23:37:17 +0100

    Kernel: Remove KerParseU16 implementation

--- src/sld/string.asm
+++ src/sld/string.asm
@@ -8,62 +8,6 @@
 section .text
 
 
-                global  KerParseU16
-KerParseU16:
-                push    bx              ; save registers
-                push    dx
-
-                xor     ax, ax          ; sum
-                xor     bx, bx          ; character
-                xor     cx, cx          ; count
-
-.LeftPad:
-                mov     bl, [si]
-                cmp     bl, ' '
-                jne     .Digit
-
-                inc     cx
-                inc     si
-                jmp    .LeftPad
-
-.Digit:
-                mov     bl, [si]
-                cmp     bl, '0'
-                jb      .BelowZero
-                cmp     bl, '9'
-                ja      .Error          ; invalid character
-
-                mov     dx, 10          ; multiplier
-                mul     dx
-                sub     bl, '0'
-                add     ax, bx          ; AX = (AX * 10) mod 65536 + BL
-
-                inc     cx
-                inc     si
-                jmp    .Digit
-
-.BelowZero:
-                cmp     bl, ' '
-                jne     .Error          ; invalid character
-
-                inc     cx
-                inc     si
-
-.RightPad:
-                mov     bl, [si]
-                cmp     bl, ' '
-                jne     .End
-
-                inc     cx
-                inc     si
-                jmp    .RightPad
-
-.Error:         stc                     ; set CF
-.End:           pop     dx              ; restore registers
-                pop     bx
-                ret
-
-
                 global  KerByteToHex
 KerByteToHex:
                 push    bx
