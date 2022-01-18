451de1781c949da2494dfdccc8cd4d6a3e7e11b4
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-18 23:02:10 +0100

    Tools: Utilize clang-format

--- tools/bits.hpp
+++ tools/bits.hpp
@@ -2,19 +2,22 @@
 
 #include <cstdint>
 
-
-inline constexpr uint64_t umaxval(unsigned bits) {
+inline constexpr uint64_t
+umaxval(unsigned bits)
+{
     return (1Ui64 << bits) - 1;
 }
 
-template<typename T>
-inline constexpr T rotl(T n, unsigned c, unsigned l = sizeof(T) * CHAR_BIT)
+template <typename T>
+inline constexpr T
+rotl(T n, unsigned c, unsigned l = sizeof(T) * CHAR_BIT)
 {
-     return (n << c | n >> (l - c)) & umaxval(l);
+    return (n << c | n >> (l - c)) & umaxval(l);
 }
 
-template<typename T>
-inline constexpr T rotr(T n, unsigned c, unsigned l = sizeof(T) * CHAR_BIT)
+template <typename T>
+inline constexpr T
+rotr(T n, unsigned c, unsigned l = sizeof(T) * CHAR_BIT)
 {
-     return (n >> c | n << (l - c)) & umaxval(l);
+    return (n >> c | n << (l - c)) & umaxval(l);
 }
