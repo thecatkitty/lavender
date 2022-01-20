83a577e6b8595389eb3728f5a900858883aba49f
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-20 22:38:28 +0100

    Video: Add glyph transformation support

--- inc/vid.h
+++ inc/vid.h
@@ -18,6 +18,7 @@ typedef struct
 {
     uint16_t CodePoint;
     char *   Overlay;
+    char *   Transformation;
     char     Base;
 } VID_CHARACTER_DESCRIPTOR;
 #pragma pack(pop)
@@ -54,5 +55,20 @@ VidConvertToLocal(uint16_t wc);
 #define ERR_VID_UNSUPPORTED ERR_CODE(ERR_FACILITY_VID, 0)
 #define ERR_VID_FAILED      ERR_CODE(ERR_FACILITY_VID, 1)
 #define ERR_VID_FORMAT      ERR_CODE(ERR_FACILITY_VID, 2)
+
+#define VID_GXF_CMD_GROW   0
+#define VID_GXF_CMD_SELECT 1
+#define VID_GXF_CMD_MOVE   2
+#define VID_GXF_CMD_CLEAR  3
+
+#ifdef __ASSEMBLER__
+
+#define GXF_GROW(n)   ((VID_GXF_CMD_GROW << 4) | (n & 0xF))
+#define GXF_SELECT(n) ((VID_GXF_CMD_SELECT << 4) | (n & 0xF))
+#define GFX_MOVE(n)   ((VID_GXF_CMD_MOVE << 4) | (n & 0xF))
+#define GFX_CLEAR(n)  ((VID_GXF_CMD_CLEAR << 4) | (n & 0xF))
+#define GFX_END       GXF_GROW(0)
+
+#endif // __ASSEMBLER__
 
 #endif // _VID_H_
