129ae547546ba3e07576714a21266c42a40063ed
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-24 02:39:01 +0100

    Kernel: Add basic music player

--- src/ker/time.c
+++ src/ker/time.c
@@ -2,6 +2,7 @@
 #include <dos.h>
 #include <stdint.h>
 
+#include <api/dos.h>
 #include <dev/pic.h>
 #include <dev/pit.h>
 #include <ker.h>
@@ -10,11 +11,23 @@
 #define KER_PIT_FREQ_DIVISOR    2048ULL
 #define KER_DELAY_MS_MULTIPLIER 100ULL
 
+#define SPKR_ENABLE 3
+
 static volatile unsigned counter = 0xFFFF;
 static isr               biosIsr;
 
+static volatile unsigned playerTicks = 0;
+static uint16_t *volatile sequence = (uint16_t *)0;
+static uint16_t *sequenceEnd;
+
+static void
+PlayerStop(void);
+
 static interrupt void far
 PitIsr(void);
+
+static void
+PlayerIsr(void);
 
 unsigned
 KerGetTicksFromMs(unsigned ms)
@@ -39,6 +52,25 @@ KerSleep(unsigned ticks)
 }
 
 void
+KerStartPlayer(void *music, uint16_t length)
+{
+    KerDisableInterrupts();
+    sequence = (uint16_t *)music;
+    sequenceEnd = sequence + length;
+    playerTicks = 0;
+    KerEnableInterrupts();
+}
+
+void
+PlayerStop(void)
+{
+    KerDisableInterrupts();
+    sequence = (uint16_t *)0;
+    _outp(0x61, _inp(0x61) & (uint8_t)~SPKR_ENABLE);
+    KerEnableInterrupts();
+}
+
+void
 PitInitChannel(unsigned channel, unsigned mode, unsigned divisor)
 {
     if (channel > 2)
@@ -56,6 +88,7 @@ PitInitChannel(unsigned channel, unsigned mode, unsigned divisor)
 void
 PitInitialize()
 {
+    counter = 0xFFFF;
     biosIsr = KerInstallIsr(PitIsr, INT_PIT);
     PitInitChannel(0, PIT_MODE_RATE_GEN, KER_PIT_FREQ_DIVISOR);
 }
@@ -63,6 +96,7 @@ PitInitialize()
 void
 PitDeinitialize()
 {
+    PlayerStop();
     KerUninstallIsr(biosIsr, INT_PIT);
     PitInitChannel(0, PIT_MODE_RATE_GEN, 0);
 }
@@ -78,6 +112,44 @@ PitIsr(void)
         biosIsr();
     }
 
+    if (0 == (counter % 10))
+    {
+        PlayerIsr();
+    }
+
     _outp(PIC1_IO_COMMAND, PIC_COMMAND_EOI);
     KerEnableInterrupts();
+}
+
+void
+PlayerIsr(void)
+{
+    if (!sequence)
+        return;
+
+    if (sequenceEnd <= sequence)
+    {
+        _outp(0x61, _inp(0x61) & (uint8_t)~SPKR_ENABLE);
+        sequence = (uint16_t *)0;
+    }
+
+    if (0 != playerTicks)
+    {
+        playerTicks--;
+        return;
+    }
+
+    sequence += 2;
+    playerTicks = sequence[0] - 1;
+
+    uint16_t divisor = sequence[1];
+    if (0 == divisor)
+    {
+        _outp(0x61, _inp(0x61) & (uint8_t)~SPKR_ENABLE);
+    }
+    else
+    {
+        PitInitChannel(2, PIT_MODE_SQUARE_WAVE_GEN, divisor);
+        _outp(0x61, _inp(0x61) | SPKR_ENABLE);
+    }
 }
