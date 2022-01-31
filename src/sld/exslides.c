0716587b86c68fd315aa07cad3f7035ad76246e7
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-01-31 11:22:56 +0100

    Slides: Move file execution to a separate function

---
+++ src/sld/exslides.c
@@ -0,0 +1,44 @@
+#include <sld.h>
+
+int
+SldExecuteFile(ZIP_LOCAL_FILE_HEADER *file, ZIP_CDIR_END_HEADER *zip)
+{
+    int   status;
+    void *data;
+    if (0 > (status = KerGetArchiveData(file, &data)))
+    {
+        return status;
+    }
+
+    const char *line = (const char *)data;
+    int         length;
+    SLD_ENTRY   entry;
+    while (line < (const char *)data + file->UncompressedSize)
+    {
+        if (0 > (length = SldLoadEntry(line, &entry)))
+        {
+            return length;
+        }
+
+        int status;
+        if (0 > (status = SldExecuteEntry(&entry, zip)))
+        {
+            return status;
+        }
+
+        if (INT_MAX == status)
+        {
+            if (0 > (length = SldFindLabel((const char *)data,
+                                           data + file->UncompressedSize,
+                                           entry.Content, &line)))
+            {
+                return length;
+            }
+            continue;
+        }
+
+        line += length;
+    }
+
+    return 0;
+}
--- src/sld/module.mk
+++ src/sld/module.mk
@@ -1,2 +1,2 @@
 ASSOURCES := $(ASSOURCES) $(DIR)/messages.S
-CCSOURCES := $(CCSOURCES) $(DIR)/exentry.c $(DIR)/ldentry.c $(DIR)/fnlabel.c
+CCSOURCES := $(CCSOURCES) $(DIR)/exslides.c $(DIR)/exentry.c $(DIR)/ldentry.c $(DIR)/fnlabel.c
