f830029fca8eee4544f20a6f65c530eeff6b72b3
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-02-06 00:19:52 +0100

    Dialog: Convert messages before displaying them

--- src/dlg/text.c
+++ src/dlg/text.c
@@ -1,11 +1,15 @@
+#include <stdlib.h>
 #include <string.h>
 
 #include <dlg.h>
+#include <ker.h>
 #include <vid.h>
 
 int
 DlgDrawText(DLG_FRAME *frame, const char *str, int line)
 {
-    return VidDrawText(str, (80 - frame->Columns) / 2,
+    char *strConv = alloca(strlen(str) + 1);
+    KerConvertFromUtf8(str, strConv, VidConvertToLocal);
+    return VidDrawText(strConv, (80 - frame->Columns) / 2,
                        (22 - frame->Lines) / 2 + line + 2);
 }
