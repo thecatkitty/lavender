2bddae0abcb47c786eb42f9ccdd111294e452d14
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-02-05 22:49:12 +0100

    Dialog: Fix vertical layout

--- src/dlg/text.c
+++ src/dlg/text.c
@@ -7,5 +7,5 @@ int
 DlgDrawText(DLG_FRAME *frame, const char *str, int line)
 {
     return VidDrawText(str, (80 - frame->Columns) / 2,
-                       (25 - frame->Lines) / 2 + line);
+                       (22 - frame->Lines) / 2 + line + 2);
 }
