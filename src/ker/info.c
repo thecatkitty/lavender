ced7af9613bafeb7cea9fba22ae1aba9e161e425
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-02-09 00:55:13 +0100

    Kernel: Look for label inside the root directory

--- src/ker/info.c
+++ src/ker/info.c
@@ -4,8 +4,8 @@
 
 #include <api/bios.h>
 #include <api/dos.h>
-#include <fmt/bootsect.h>
 #include <fmt/exe.h>
+#include <fmt/fat.h>
 #include <ker.h>
 
 extern DOS_PSP *KerPsp;
@@ -129,27 +129,32 @@ KerGetFloppyDriveCount(void)
 int
 KerGetVolumeInfo(uint8_t drive, KER_VOLUME_INFO *out)
 {
-    BOOT_SECTOR bs;
-    if (0 != DosReadDiskAbsolute(drive, 1, 0, (char *)&bs))
+    union {
+        char                Bytes[512];
+        BOOT_SECTOR         Boot;
+        FAT_DIRECTORY_ENTRY Root[512 / sizeof(FAT_DIRECTORY_ENTRY)];
+    } sector;
+
+    if (0 != DosReadDiskAbsolute(drive, 1, 0, sector.Bytes))
     {
         ERR(KER_DISK_ACCESS);
     }
 
-    if (0xAA55U != bs.Magic)
+    if (0xAA55U != sector.Boot.Magic)
     {
         ERR(KER_UNSUPPORTED);
     }
 
     int offset, size;
-    switch ((uint8_t)bs.Jump[0])
+    switch ((uint8_t)sector.Boot.Jump[0])
     {
     case 0xEB: // JMP rel8
-        offset = *(int8_t *)(bs.Jump + 1);
-        size = offset - (sizeof(bs.OemString) + 1);
+        offset = *(int8_t *)(sector.Boot.Jump + 1);
+        size = offset - (sizeof(sector.Boot.OemString) + 1);
         break;
     case 0xE9: // JMP rel16
-        offset = *(int16_t *)(bs.Jump + 1);
-        size = offset - sizeof(bs.OemString);
+        offset = *(int16_t *)(sector.Boot.Jump + 1);
+        size = offset - sizeof(sector.Boot.OemString);
         break;
     default:
         ERR(KER_UNSUPPORTED);
@@ -165,25 +170,25 @@ KerGetVolumeInfo(uint8_t drive, KER_VOLUME_INFO *out)
         out->Label[0] = 0;
         break;
     case sizeof(BPB_DOS34): {
-        BPB_DOS34 *bpb = (BPB_DOS34 *)bs.Payload;
+        BPB_DOS34 *bpb = (BPB_DOS34 *)sector.Boot.Payload;
         out->SerialNumber = bpb->SerialNumber;
         out->Label[0] = 0;
         break;
     }
     case sizeof(BPB_DOS40): {
-        BPB_DOS40 *bpb = (BPB_DOS40 *)bs.Payload;
+        BPB_DOS40 *bpb = (BPB_DOS40 *)sector.Boot.Payload;
         out->SerialNumber = bpb->Bpb34.SerialNumber;
         CopyVolumeLabel(out->Label, bpb->Label);
         break;
     }
     case sizeof(BPB_DOS71): {
-        BPB_DOS71 *bpb = (BPB_DOS71 *)bs.Payload;
+        BPB_DOS71 *bpb = (BPB_DOS71 *)sector.Boot.Payload;
         out->SerialNumber = bpb->SerialNumber;
         out->Label[0] = 0;
         break;
     }
     case sizeof(BPB_DOS71_FULL): {
-        BPB_DOS71_FULL *bpb = (BPB_DOS71_FULL *)bs.Payload;
+        BPB_DOS71_FULL *bpb = (BPB_DOS71_FULL *)sector.Boot.Payload;
         out->SerialNumber = bpb->Bpb71.SerialNumber;
         CopyVolumeLabel(out->Label, bpb->Label);
         break;
@@ -192,6 +197,29 @@ KerGetVolumeInfo(uint8_t drive, KER_VOLUME_INFO *out)
         ERR(KER_UNSUPPORTED);
     }
 
+    BPB_DOS20 *bpb = (BPB_DOS20 *)sector.Boot.Payload;
+    uint16_t   rootDirEntries = bpb->RootEntries;
+    uint16_t rootDirSectors = ((rootDirEntries * sizeof(FAT_DIRECTORY_ENTRY)) +
+                               (bpb->BytesPerSector - 1)) /
+                              bpb->BytesPerSector;
+    uint16_t firstDataSector = bpb->ReservedSectors +
+                               (bpb->Fats * bpb->SectorsPerFat) +
+                               rootDirSectors;
+    uint16_t firstRootDirSector = firstDataSector - rootDirSectors;
+
+    if (0 != DosReadDiskAbsolute(drive, 1, firstRootDirSector, sector.Bytes))
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < (sizeof(sector) / sizeof(FAT_DIRECTORY_ENTRY)); i++)
+    {
+        if (FAT_ATTRIBUTE_VOLUME_ID != sector.Root[i].Attributes)
+            continue;
+
+        CopyVolumeLabel(out->Label, sector.Root[i].FileName);
+        break;
+    }
     return 0;
 }
 
