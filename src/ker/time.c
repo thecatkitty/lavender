b4013ac6b5526ffccef0c0ae3f36e764ccfd6757
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-06-16 19:55:50 +0200

    pal: dospc: move time keeping code to PAL

--- src/ker/time.c
+++ src/ker/time.c
@@ -9,43 +9,14 @@
 #include <ker.h>
 #include <pal.h>
 
-#define KER_PIT_INPUT_FREQ      11931816667ULL
-#define KER_PIT_FREQ_DIVISOR    2048ULL
-#define KER_DELAY_MS_MULTIPLIER 100ULL
-
-static volatile uint32_t s_Counter;
-static ISR               s_BiosIsr;
-
 static volatile unsigned s_PlayerTicks = 0;
 static SPK_NOTE3 *volatile s_Sequence = (SPK_NOTE3 *)0;
 
 static void
 PlayerStop(void);
 
-static interrupt void far
-PitIsr(void);
-
 static void
 PlayerIsr(void);
-
-unsigned
-KerGetTicksFromMs(unsigned ms)
-{
-    uint32_t ticks = (uint32_t)ms * KER_DELAY_MS_MULTIPLIER;
-    ticks /= (10000000UL * KER_DELAY_MS_MULTIPLIER) * KER_PIT_FREQ_DIVISOR /
-             KER_PIT_INPUT_FREQ;
-    return ticks;
-}
-
-void
-KerSleep(unsigned ticks)
-{
-    uint32_t until = s_Counter + ticks;
-    while (s_Counter != until)
-    {
-        asm("hlt");
-    }
-}
 
 void
 KerStartPlayer(void *music, uint16_t length)
@@ -66,65 +37,7 @@ PlayerStop(void)
 }
 
 void
-PitInitChannel(unsigned channel, unsigned mode, unsigned divisor)
-{
-    if (channel > 2)
-        return;
-    mode &= PIT_MODE_MASK;
-
-    _disable();
-    _outp(PIT_IO_COMMAND, (mode << PIT_MODE) | (channel << PIT_CHANNEL) |
-                              (1 << PIT_BYTE_HI) | (1 << PIT_BYTE_LO));
-    _outp(PIT_IO + channel, divisor & 0xFF);
-    _outp(PIT_IO + channel, divisor >> 8);
-    _enable();
-}
-
-void
-PitInitialize()
-{
-    _disable();
-    s_BiosIsr = _dos_getvect(INT_PIT);
-    _dos_setvect(INT_PIT, PitIsr);
-    _enable();
-
-    s_Counter = 0;
-    PitInitChannel(0, PIT_MODE_RATE_GEN, KER_PIT_FREQ_DIVISOR);
-}
-
-void
-PitDeinitialize()
-{
-    PlayerStop();
-    _dos_setvect(INT_PIT, s_BiosIsr);
-    PitInitChannel(0, PIT_MODE_RATE_GEN, 0);
-}
-
-interrupt void far
-PitIsr(void)
-{
-    _disable();
-    s_Counter++;
-
-    if (0 == (s_Counter % 10))
-    {
-        PlayerIsr();
-    }
-
-    if (0 == (s_Counter % 32))
-    {
-        s_BiosIsr();
-    }
-    else
-    {
-        _outp(PIC1_IO_COMMAND, PIC_COMMAND_EOI);
-    }
-
-    _enable();
-}
-
-void
-PlayerIsr(void)
+PlayerIsr()
 {
     if (!s_Sequence)
         return;
--- src/pal/dospc.c
+++ src/pal/dospc.c
@@ -3,28 +3,59 @@
 #include <string.h>
 
 #include <api/dos.h>
+#include <dev/pic.h>
 #include <dev/pit.h>
 #include <ker.h>
 #include <pal.h>
 
-#define SPKR_ENABLE 3
+#define SPKR_ENABLE         3
+#define PIT_INPUT_FREQ      11931816667ULL
+#define PIT_FREQ_DIVISOR    2048ULL
+#define DELAY_MS_MULTIPLIER 100ULL
 
 extern char __edata[], __sbss[], __ebss[];
 
-extern void
-PitInitialize(void);
-
-extern void
-PitDeinitialize(void);
-
-extern void
-PitInitChannel(unsigned channel, unsigned mode, unsigned divisor);
+static volatile uint32_t _counter;
+static ISR               _bios_isr;
 
 #ifdef STACK_PROFILING
 static uint64_t      *_stack_start;
 static uint64_t      *_stack_end = (uint64_t *)0xFFE8;
 static const uint64_t STACK_FILL_PATTERN = 0x0123456789ABCDEFULL;
 #endif // STACK_PROFILING
+
+static void
+_pit_init_channel(unsigned channel, unsigned mode, unsigned divisor)
+{
+    if (channel > 2)
+        return;
+    mode &= PIT_MODE_MASK;
+
+    _disable();
+    _outp(PIT_IO_COMMAND, (mode << PIT_MODE) | (channel << PIT_CHANNEL) |
+                              (1 << PIT_BYTE_HI) | (1 << PIT_BYTE_LO));
+    _outp(PIT_IO + channel, divisor & 0xFF);
+    _outp(PIT_IO + channel, divisor >> 8);
+    _enable();
+}
+
+static void far interrupt
+_pit_isr(void)
+{
+    _disable();
+    _counter++;
+
+    if (0 == (_counter & 0x11111))
+    {
+        _bios_isr();
+    }
+    else
+    {
+        _outp(PIC1_IO_COMMAND, PIC_COMMAND_EOI);
+    }
+
+    _enable();
+}
 
 void
 pal_initialize(ZIP_CDIR_END_HEADER **zip)
@@ -43,13 +74,22 @@ pal_initialize(ZIP_CDIR_END_HEADER **zip)
         KerTerminate(-status);
     }
 
-    PitInitialize();
+    _disable();
+    _bios_isr = _dos_getvect(INT_PIT);
+    _dos_setvect(INT_PIT, _pit_isr);
+    _enable();
+
+    _counter = 0;
+    _pit_init_channel(0, PIT_MODE_RATE_GEN, PIT_FREQ_DIVISOR);
 }
 
 void
 pal_cleanup(void)
 {
-    PitDeinitialize();
+    _dos_setvect(INT_PIT, _bios_isr);
+    _pit_init_channel(0, PIT_MODE_RATE_GEN, 0);
+
+    nosound();
 
 #ifdef STACK_PROFILING
     uint64_t *untouched;
@@ -73,8 +113,22 @@ pal_cleanup(void)
 }
 
 void
+pal_sleep(unsigned ms)
+{
+    uint32_t ticks = (uint32_t)ms * DELAY_MS_MULTIPLIER;
+    ticks /=
+        (10000000UL * DELAY_MS_MULTIPLIER) * PIT_FREQ_DIVISOR / PIT_INPUT_FREQ;
+
+    uint32_t until = _counter + ticks;
+    while (_counter != until)
+    {
+        asm("hlt");
+    }
+}
+
+void
 pal_beep(uint16_t divisor)
 {
-    PitInitChannel(2, PIT_MODE_SQUARE_WAVE_GEN, divisor);
+    _pit_init_channel(2, PIT_MODE_SQUARE_WAVE_GEN, divisor);
     _outp(0x61, _inp(0x61) | SPKR_ENABLE);
 }
