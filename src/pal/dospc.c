4c5a3e57a4177feb2c975f94dc98859c05249f09
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-06-17 11:55:24 +0200

    pal: dospc: move DOS/NT detection to PAL

--- src/pal/dospc.c
+++ src/pal/dospc.c
@@ -1,13 +1,16 @@
 #include <conio.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <api/bios.h>
 #include <api/dos.h>
 #include <dev/pic.h>
 #include <dev/pit.h>
+#include <fmt/exe.h>
 #include <fmt/zip.h>
 #include <ker.h>
 #include <pal.h>
@@ -27,7 +30,6 @@ typedef struct
 
 extern char __edata[], __sbss[], __ebss[];
 extern char _binary_obj_version_txt_start[];
-
 
 extern const char __serrf[];
 extern const char __serrm[];
@@ -99,7 +101,7 @@ _pit_isr(void)
 
 // Find a message using its key byte
 //   WILL CRASH IF MESSAGE NOT FOUND!
-const char *
+static const char *
 _find_message(const char *messages, unsigned key)
 {
     while (true)
@@ -122,7 +124,7 @@ static void
 _die_errno(void)
 {
     DosPutS(StrKerError);
-        
+
     char code[10];
     itoa(errno, code, 10);
     code[strlen(code)] = '$';
@@ -142,21 +144,84 @@ _die_status(int error)
     DosExit(error);
 }
 
+static bool
+_is_dos(uint8_t major)
+{
+    uint8_t dosMajor = DosGetVersion() & 0xFF;
+    return (1 == major) ? (0 == dosMajor) : (major == dosMajor);
+}
+
+static bool
+_is_winnt(void)
+{
+    // NTVDM claims to be DOS 5
+    if (!_is_dos(5))
+    {
+        return false; // It's not 5, so it's not NTVDM.
+    }
+
+    // Does it have %OS% environment variable?
+    const char *os = getenv("OS");
+    if (NULL == os)
+    {
+        return false; // It's just a regular DOS 5.
+    }
+
+    // Does it claim to be Windows NT?
+    return 0 == strcmp(os, "Windows_NT");
+}
+
+static uint16_t
+_get_winnt_version(void)
+{
+    const char *sysroot = getenv("SYSTEMROOT");
+    if (NULL == sysroot)
+    {
+        return 0; // No system root
+    }
+
+    char smss[261];
+    strcpy(smss, sysroot);
+    strcat(smss, "\\system32\\smss.exe");
+
+    int fd;
+    if (0 > (fd = open(smss, O_RDONLY)))
+    {
+        return 0; // No SMSS
+    }
+
+    union {
+        EXE_DOS_HEADER         DosHdr;
+        ULONG                  NewSignature;
+        EXE_PE_OPTIONAL_HEADER OptionalHeader;
+    } data;
+
+    read(fd, &data, sizeof(EXE_DOS_HEADER));
+    if (0x5A4D != data.DosHdr.e_magic)
+    {
+        return 0; // Invalid executable
+    }
+
+    lseek(fd, data.DosHdr.e_lfanew, SEEK_SET);
+    read(fd, &data, sizeof(ULONG));
+    if (0x00004550 != data.NewSignature)
+    {
+        return 0; // Invalid executable
+    }
+
+    lseek(fd, sizeof(EXE_PE_FILE_HEADER), SEEK_CUR);
+    read(fd, &data, sizeof(EXE_PE_OPTIONAL_HEADER));
+    close(fd);
+
+    return (data.OptionalHeader.MajorImageVersion << 8) |
+           data.OptionalHeader.MinorImageVersion;
+}
+
 bool
 _is_compatible(void)
 {
-    if (KerIsDosMajor(1))
-    {
-        // We're using DOS 2.0+ API here, so that's unfortunate.
-        return false;
-    }
-
-    if (!KerIsWindowsNt())
-    {
-        return true;
-    }
-
-    return 0x0600 > KerGetWindowsNtVersion();
+    // We're using DOS 2.0+ API here, and we can't run on Vista and above.
+    return !_is_dos(1) && (!_is_winnt() || (0x0600 > _get_winnt_version()));
 }
 
 void
