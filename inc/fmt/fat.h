69a62026234e23cfedbe57463985d18f6205ff06
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-06-28 20:47:33 +0200

    fmt: convert FAT definitions to snake_case

--- inc/fmt/fat.h
+++ inc/fmt/fat.h
@@ -23,9 +23,9 @@ typedef struct
     char     OemId[8];
     char     Payload[512 - (3 + 8 + sizeof(uint16_t))];
     uint16_t Magic;
-} BOOT_SECTOR;
+} fat_boot_sector;
 
-static_assert(512 == sizeof(BOOT_SECTOR),
+static_assert(512 == sizeof(fat_boot_sector),
               "Boot sector size doesn't match specification");
 
 typedef struct
@@ -38,68 +38,68 @@ typedef struct
     uint16_t Sectors;
     uint8_t  Media;
     uint16_t SectorsPerFat;
-} BPB_DOS20;
+} fat_bpb20;
 
-static_assert(13 == sizeof(BPB_DOS20),
+static_assert(13 == sizeof(fat_bpb20),
               "DOS 2.0 BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS20 Bpb20;
+    fat_bpb20 bpb20;
     uint16_t  SectorsPerTrack;
     uint16_t  Heads;
     uint16_t  HiddenSectors;
-} BPB_DOS30;
+} fat_bpb30;
 
-static_assert(19 == sizeof(BPB_DOS30),
+static_assert(19 == sizeof(fat_bpb30),
               "DOS 3.0 BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS30 Bpb30;
+    fat_bpb30 bpb30;
     uint16_t  Sectors;
-} BPB_DOS32;
+} fat_bpb32;
 
-static_assert(21 == sizeof(BPB_DOS32),
+static_assert(21 == sizeof(fat_bpb32),
               "DOS 3.2 BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS20 Bpb20;
+    fat_bpb20 bpb20;
     uint16_t  SectorsPerTrack;
     uint16_t  Heads;
     uint32_t  HiddenSectors;
     uint32_t  LargeSectors;
-} BPB_DOS33;
+} fat_bpb33;
 
-static_assert(25 == sizeof(BPB_DOS33),
+static_assert(25 == sizeof(fat_bpb33),
               "DOS 3.3 BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS33 Bpb33;
+    fat_bpb33 bpb33;
     uint8_t   PhysicalDriveNumber;
     uint8_t   CurrentHead;
     uint8_t   Signature;
     uint32_t  Id;
-} BPB_DOS34;
+} fat_bpb34;
 
-static_assert(32 == sizeof(BPB_DOS34),
+static_assert(32 == sizeof(fat_bpb34),
               "DOS 3.4 BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS34 Bpb34;
+    fat_bpb34 bpb34;
     char      FatLabel[11];
     char      SystemId[8];
-} BPB_DOS40;
+} fat_bpb40;
 
-static_assert(51 == sizeof(BPB_DOS40),
+static_assert(51 == sizeof(fat_bpb40),
               "DOS 4.0 BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS33 Bpb33;
+    fat_bpb33 bpb33;
     uint32_t  LargeSectorsPerFat;
     uint16_t  ExtendedFlags;
     uint16_t  FsVersion;
@@ -111,19 +111,19 @@ typedef struct
     uint8_t   CurrentHead;
     uint8_t   Signature;
     uint32_t  Id;
-} BPB_DOS71;
+} fat_bpb71;
 
-static_assert(60 == sizeof(BPB_DOS71),
+static_assert(60 == sizeof(fat_bpb71),
               "DOS 7.1 short BPB size doesn't match specification");
 
 typedef struct
 {
-    BPB_DOS71 Bpb71;
+    fat_bpb71 bpb71;
     char      FatLabel[11];
     char      SystemId[8];
-} BPB_DOS71_FULL;
+} fat_bpb71_full;
 
-static_assert(79 == sizeof(BPB_DOS71_FULL),
+static_assert(79 == sizeof(fat_bpb71_full),
               "DOS 7.1 full BPB size doesn't match specification");
 
 typedef struct
@@ -131,9 +131,9 @@ typedef struct
     unsigned DoubleSecond : 5;
     unsigned Minute : 6;
     unsigned Hour : 5;
-} FAT_TIME;
+} fat_time;
 
-static_assert(sizeof(uint16_t) == sizeof(FAT_TIME),
+static_assert(sizeof(uint16_t) == sizeof(fat_time),
               "FAT time struct size doesn't match specification");
 
 typedef struct
@@ -141,18 +141,18 @@ typedef struct
     unsigned Day : 5;
     unsigned Month : 4;
     unsigned Year : 7;
-} FAT_DATE;
+} fat_date;
 
-static_assert(sizeof(uint16_t) == sizeof(FAT_DATE),
+static_assert(sizeof(uint16_t) == sizeof(fat_date),
               "FAT date struct size doesn't match specification");
 
 typedef struct
 {
-    FAT_TIME Time;
-    FAT_DATE Date;
-} FAT_DATE_TIME;
+    fat_time Time;
+    fat_date Date;
+} fat_date_time;
 
-static_assert(sizeof(uint32_t) == sizeof(FAT_DATE_TIME),
+static_assert(sizeof(uint32_t) == sizeof(fat_date_time),
               "FAT date and time struct size doesn't match specification");
 
 typedef struct
@@ -161,15 +161,15 @@ typedef struct
     uint8_t       Attributes;
     uint8_t       CaseFlag;
     uint8_t       CreateMillisecond;
-    FAT_DATE_TIME FileCreateTime;
-    FAT_DATE      FileLastAccess;
+    fat_date_time FileCreateTime;
+    fat_date      FileLastAccess;
     uint16_t      FileClusterHigh;
-    FAT_DATE_TIME FileModificationTime;
+    fat_date_time FileModificationTime;
     uint16_t      FileCluster;
     uint32_t      FileSize;
-} FAT_DIRECTORY_ENTRY;
+} fat_directory_entry;
 
-static_assert(32 == sizeof(FAT_DIRECTORY_ENTRY),
+static_assert(32 == sizeof(fat_directory_entry),
               "FAT directory entry size doesn't match specification");
 
 #pragma pack(pop)
