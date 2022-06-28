6d9029d52dce3deeeb7208b6e2baa8f5e5c3bfed
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-06-28 19:35:53 +0200

    pal: dospc: add timer handler support

--- inc/pal.h
+++ inc/pal.h
@@ -5,7 +5,15 @@
 
 #include <base.h>
 
+#define DEFINE_HANDLE(x)                                                       \
+    typedef struct                                                             \
+    {                                                                          \
+    } * x
+
 DEFINE_HANDLE(hasset);
+DEFINE_HANDLE(htimer);
+
+typedef void (*pal_timer_callback)(void *context);
 
 extern void
 pal_initialize(void);
@@ -36,5 +44,11 @@ pal_get_version_string(void);
 
 extern uint32_t
 pal_get_medium_id(const char *tag);
+
+extern htimer
+pal_register_timer_callback(pal_timer_callback callback, void *context);
+
+extern bool
+pal_unregister_timer_callback(htimer timer);
 
 #endif // _PAL_H_
