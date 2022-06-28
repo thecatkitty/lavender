d4160f7d495165295331eff88843152510fc01c4
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-06-28 20:09:10 +0200

    pal: dospc: merge PIC and PIT definitions into PAL

--- src/pal/dospc.h
+++ src/pal/dospc.h
@@ -1,10 +1,21 @@
-#ifndef _DEV_PIT_H_
-#define _DEV_PIT_H_
+#ifndef _DOSPC_H_
+#define _DOSPC_H_
+
+// Programmable Interrupt Controller definitions
+#define PIC1_IO         0x20
+#define PICx_IO_COMMAND 0
+#define PICx_IO_DATA    1
+
+#define PIC1_IO_COMMAND (PIC1_IO + PICx_IO_COMMAND)
+#define PIC1_IO_DATA    (PIC1_IO + PICx_IO_DATA)
+
+#define PIC_COMMAND_EOI 0x20
 
 #define INT_PIT        0x08
 #define PIT_IO         0x40
 #define PIT_IO_COMMAND (PIT_IO + 3)
 
+// Programmable Interval Timer definitions
 #define PIT_CHANNEL      6
 #define PIT_READ_BACK    3
 #define PIT_CHANNEL_MASK 0x03
@@ -23,4 +34,10 @@
 
 #define PIT_BCD 0
 
-#endif // _DEV_PIT_H_
+#define PIT_INPUT_FREQ      11931816667ULL
+#define PIT_FREQ_DIVISOR    2048ULL
+
+// PC Speaker definitions
+#define SPKR_ENABLE         3
+
+#endif // _DOSPC_H_
