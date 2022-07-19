953958cc14352e88a15a129e41597909300d1ace
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-07-19 21:49:40 +0200

    crg: introduce an extensible decryption architecture

---
+++ src/crg/crg_impl.h
@@ -0,0 +1,19 @@
+#ifndef _CRG_IMPL_H_
+#define _CRG_IMPL_H_
+
+#include <crg.h>
+
+typedef uint8_t (*crg_stream_at)(crg_stream *stream, size_t i);
+typedef bool (*crg_stream_decrypt)(crg_stream *stream, uint8_t *dst);
+typedef bool (*crg_stream_validate)(crg_stream *stream, uint32_t crc);
+
+typedef struct
+{
+    crg_stream_at       at;
+    crg_stream_decrypt  decrypt;
+    crg_stream_validate validate;
+} crg_stream_impl;
+
+extern crg_stream_impl __crg_xor_impl;
+
+#endif // _CRG_IMPL_H_
--- src/crg/module.mk
+++ src/crg/module.mk
@@ -1,2 +1,2 @@
 ASSOURCES := $(ASSOURCES) $(DIR)/messages.S
-CCSOURCES := $(CCSOURCES) $(DIR)/prompt.c $(DIR)/splitkey.c $(DIR)/xor.c
+CCSOURCES := $(CCSOURCES) $(DIR)/crg.c $(DIR)/prompt.c $(DIR)/splitkey.c $(DIR)/xor.c
