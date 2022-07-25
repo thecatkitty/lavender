99b84efba7f04086358603aa110b33d04e7ce7a8
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-07-25 22:26:21 +0200

    sld: extract text execution to a new file

--- src/sld/module.mk
+++ src/sld/module.mk
@@ -1,2 +1,9 @@
 ASSOURCES := $(ASSOURCES) $(DIR)/messages.S
-CCSOURCES := $(CCSOURCES) $(DIR)/rnscript.c $(DIR)/exentry.c $(DIR)/ldentry.c $(DIR)/fnlabel.c $(DIR)/bitmap.c $(DIR)/protect.c
+CCSOURCES := $(CCSOURCES) \
+	$(DIR)/rnscript.c \
+	$(DIR)/exentry.c \
+	$(DIR)/ldentry.c \
+	$(DIR)/fnlabel.c \
+	$(DIR)/bitmap.c \
+	$(DIR)/text.c \
+	$(DIR)/protect.c
