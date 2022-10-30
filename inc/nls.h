46e9024f1262cd020c5bfd8066659f3955202571
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-10-30 20:26:25 +0100

    nls: remove dead code used previously by assembler

--- inc/nls.h
+++ inc/nls.h
@@ -5,28 +5,4 @@
 #define LCID_ENU 1033
 #define LCID_PLK 1045
 
-#ifndef LANG
-#define LANG LCID_ENU
-#endif
-
-#if LANG == LCID_CSY
-#define IF_LANG_CSY(x) x
-#else
-#define IF_LANG_CSY(x)
-#endif
-
-#if LANG == LCID_ENU
-#define IF_LANG_ENU(x) x
-#else
-#define IF_LANG_ENU(x)
-#endif
-
-#if LANG == LCID_PLK
-#define IF_LANG_PLK(x) x
-#else
-#define IF_LANG_PLK(x)
-#endif
-
-#define IF_LANG(lcid, x) IF_LANG_##lcid(x)
-
 #endif // _NLS_H_
