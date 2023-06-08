22f3e23f931171c7fe05ebb2d3432e7c05db7bf8
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-06-08 21:14:20 +0200

    api: msmouse: add Microsoft Mouse basic API definitions

---
+++ inc/api/msmouse.h
@@ -0,0 +1,34 @@
+#ifndef _API_MSMOUSE_H
+#define _API_MSMOUSE_H
+
+#include <base.h>
+
+static inline bool
+msmouse_init(void)
+{
+    uint16_t ax;
+    asm volatile("int $0x33" : "=a"(ax) : "a"(0x0000) : "b");
+    return 0xFFFF == ax;
+}
+
+static inline void
+msmouse_show(void)
+{
+    asm volatile("int $0x33" : : "a"(0x0001));
+}
+
+static inline void
+msmouse_hide(void)
+{
+    asm volatile("int $0x33" : : "a"(0x0002));
+}
+
+static inline uint16_t
+msmouse_get_status(uint16_t *x, uint16_t *y)
+{
+    uint16_t bx;
+    asm volatile("int $0x33" : "=b"(bx), "=c"(*x), "=d"(*y) : "a"(0x0003));
+    return bx;
+}
+
+#endif // _API_MSMOUSE_H
