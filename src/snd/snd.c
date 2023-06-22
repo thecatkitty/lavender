868b7d1c4852806af9c7e26aec19159424ab476d
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-06-22 21:27:42 +0200

    snd: handle sound playback in the main loop

--- src/snd/snd.c
+++ src/snd/snd.c
@@ -5,12 +5,22 @@
 static volatile unsigned _ticks = 0;
 static spk_note3 *volatile _sequence = (spk_note3 *)0;
 
+static uint32_t _tick_rate;
+static uint32_t _next_tick;
+
 void
-__snd_timer_callback(void)
+snd_handle(void)
 {
     if (!_sequence)
         return;
 
+    uint32_t now = pal_get_counter();
+    if (_next_tick > now)
+    {
+        return;
+    }
+
+    _next_tick = now + _tick_rate;
     if (0 != _ticks)
     {
         _ticks--;
@@ -39,6 +49,8 @@ __snd_timer_callback(void)
 void
 snd_play(void *music, uint16_t length)
 {
+    spk_header *header = (spk_header *)music;
+    _tick_rate = pal_get_ticks(1000 / header->ticks_per_second);
     _sequence = (spk_note3 *)((uint8_t *)music + sizeof(spk_header));
     _ticks = 0;
 }
