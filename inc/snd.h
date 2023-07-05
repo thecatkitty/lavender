ba9291818d63bef6522a8cfe0462a2c66b458ee0
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-07-05 23:17:56 +0200

    snd: pcspk: allow driving speaker with MIDI messages

--- inc/snd.h
+++ inc/snd.h
@@ -2,6 +2,7 @@
 #define _SND_H_
 
 #include <base.h>
+#include <fmt/midi.h>
 
 // Start playing music
 extern void
@@ -18,5 +19,9 @@ snd_silence(void);
 // Handle sound related services
 extern void
 snd_handle(void);
+
+// Send MIDI message
+extern void
+snd_send(const char *msg, size_t length);
 
 #endif // _SND_H_
--- src/snd/pcspk.c
+++ src/snd/pcspk.c
@@ -1,6 +1,27 @@
 #include <platform/dospc.h>
 #include <snd.h>
 
+#define KEY_MIN          14 // PIT incapable of frequencies lower than D0
+#define KEY_A4           69
+#define FREQ_A4          440
+#define INPUT_FREQ_U32F8 305454507UL
+
+// As 8-bit fractional part; integer part always 1
+static const uint8_t KEY_MULTIPLIERS[] = {
+    0,   // A   .000
+    15,  // A#  .059
+    31,  // B   .122
+    48,  // C   .189
+    67,  // C#  .260
+    86,  // D   .335
+    106, // D#  .414
+    128, // E   .498
+    150, // F   .587
+    175, // F#  .682
+    200, // G   .782
+    227  // G#  .888
+};
+
 void
 snd_beep(uint16_t divisor)
 {
@@ -11,4 +32,70 @@ void
 snd_silence(void)
 {
     dospc_silence();
+}
+
+void
+snd_send(const char *msg, size_t length)
+{
+    uint8_t status = msg[0];
+
+    uint8_t channel = status & 0xF;
+    if (0xF0 != (status & 0xF0))
+    {
+        status &= 0xF0;
+    }
+    else
+    {
+        channel = 0;
+    }
+
+    if (0 != channel)
+    {
+        return;
+    }
+
+    switch (status)
+    {
+    case MIDI_MSG_NOTEOFF:
+    case MIDI_MSG_NOTEON: {
+        uint8_t key = msg[1];
+        uint8_t velocity = msg[2];
+
+        if ((MIDI_MAX_KEY < key) || (MIDI_MAX_VELOCITY < velocity))
+        {
+            return;
+        }
+
+        if ((MIDI_MSG_NOTEOFF == status) || (0 == velocity))
+        {
+            dospc_silence();
+            return;
+        }
+
+        if (KEY_MIN > key)
+        {
+            return;
+        }
+
+        uint32_t freq = FREQ_A4;
+        if (KEY_A4 < key)
+        {
+            uint8_t delta = key - KEY_A4;
+            freq <<= delta / 12;
+            freq *= 256 + KEY_MULTIPLIERS[delta % 12];
+        }
+        else
+        {
+            uint8_t delta = KEY_A4 - key;
+            freq <<= 16;
+            freq >>= delta / 12;
+            freq /= 256 + KEY_MULTIPLIERS[delta % 12];
+        }
+
+        // freq is u32f8
+        uint16_t divisor = INPUT_FREQ_U32F8 / freq;
+        dospc_beep(divisor);
+        break;
+    }
+    }
 }
