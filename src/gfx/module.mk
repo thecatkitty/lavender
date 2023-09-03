175357c31b343dfa1b02011f01ebac1e6b12022b
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-09-03 14:07:53 +0200

    gfx: sdl2: extract SDL2 graphics driver

--- src/gfx/module.mk
+++ src/gfx/module.mk
@@ -1,3 +1,6 @@
 ifeq ($(findstring dospc,$(LAV_TARGET)),dospc)
 CCSOURCES := $(CCSOURCES) $(DIR)/cga.c $(DIR)/vbe.c $(DIR)/font-8x8.c
 endif
+ifeq ($(findstring test,$(LAV_TARGET)),test)
+CCSOURCES := $(CCSOURCES) $(DIR)/sdl2.c
+endif
