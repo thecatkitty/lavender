ff5f4b197eb69cd08105e4ad56844a48a29cd9c6
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-10-10 20:01:27 +0200

    gfx: add routine for retrieving colors by name

---
+++ src/gfx/color.c
@@ -0,0 +1,54 @@
+#include <string.h>
+
+#include <gfx.h>
+#include <pal.h>
+
+typedef struct
+{
+    const char *name;
+    gfx_color   color;
+} color_name;
+
+static const color_name NAMED_COLORS[] = {
+    {"aqua", GFX_COLOR_CYAN},       {"black", GFX_COLOR_BLACK},
+    {"blue", GFX_COLOR_BLUE},       {"cyan", GFX_COLOR_CYAN},
+    {"fuchsia", GFX_COLOR_FUCHSIA}, {"gray", GFX_COLOR_GRAY},
+    {"green", GFX_COLOR_GREEN},     {"lime", GFX_COLOR_LIME},
+    {"maroon", GFX_COLOR_MAROON},   {"navy", GFX_COLOR_NAVY},
+    {"olive", GFX_COLOR_OLIVE},     {"purple", GFX_COLOR_PURPLE},
+    {"red", GFX_COLOR_RED},         {"silver", GFX_COLOR_SILVER},
+    {"teal", GFX_COLOR_TEAL},       {"white", GFX_COLOR_WHITE},
+    {"yellow", GFX_COLOR_YELLOW},
+};
+
+gfx_color
+gfx_get_color(const char *str)
+{
+    size_t begin = 0;
+    size_t end = lengthof(NAMED_COLORS) - 1;
+    size_t i;
+
+    int result = -1;
+    while (begin <= end)
+    {
+        i = (begin + end) / 2;
+
+        result = strcmp(NAMED_COLORS[i].name, str);
+        if (0 < result)
+        {
+            end = i - 1;
+        }
+        else if (0 > result)
+        {
+            begin = i + 1;
+        }
+        else
+        {
+            LOG("exit, %d", NAMED_COLORS[i].color);
+            return NAMED_COLORS[i].color;
+        }
+    }
+
+    LOG("exit, unknown!");
+    return GFX_COLOR_UNKNOWN;
+}
--- src/gfx/module.mk
+++ src/gfx/module.mk
@@ -1,3 +1,5 @@
+CCSOURCES := $(CCSOURCES) $(DIR)/color.c
+
 ifeq ($(findstring dospc,$(LAV_TARGET)),dospc)
 CCSOURCES := $(CCSOURCES) $(DIR)/cga.c $(DIR)/vbe.c $(DIR)/font-8x8.c
 endif
