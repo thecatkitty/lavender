dd87905a4bf6c867ba55e3f5841691b20bb11cbd
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-10-14 21:56:17 +0200

    dlg: add Windows prompt dialog boxes

--- inc/platform/windows.h
+++ inc/platform/windows.h
@@ -1,6 +1,9 @@
 #ifndef _PLATFORM_WINDOWS_H_
 #define _PLATFORM_WINDOWS_H_
 
+#ifndef WINVER
+#define WINVER 0x0500
+#endif
 #include <windows.h>
 
 extern HWND
--- src/dlg/windows.c
+++ src/dlg/windows.c
@@ -5,6 +5,7 @@
 #include <fmt/utf8.h>
 #include <gfx.h>
 #include <pal.h>
+#include <platform/sdl2arch.h>
 #include <platform/windows.h>
 
 enum
@@ -14,31 +15,20 @@ enum
     STATE_PROMPT
 };
 
-enum
-{
-    STATE_PROMPT_NONE = STATE_PROMPT | (0 << 8),
-    STATE_PROMPT_INVALID1 = STATE_PROMPT | (1 << 8),
-    STATE_PROMPT_INVALID2 = STATE_PROMPT | (2 << 8),
-    STATE_PROMPT_INVALID3 = STATE_PROMPT | (3 << 8)
-};
-
 static gfx_dimensions _screen = {0, 0};
-static gfx_dimensions _glyph = {0, 0};
 static int            _state = STATE_NONE;
-static uint32_t       _blink_start;
-static char          *_buffer;
-static int            _size;
-static dlg_validator  _validator;
-static int            _cursor;
-static int            _field_left;
-static int            _field_top;
-static gfx_dimensions _box;
-static int            _box_top;
-static int            _box_left;
+
+static HGLOBAL           _hgbl = NULL;
+static NONCLIENTMETRICSW _nclm = {0};
 
 static void
 _draw_background(void)
 {
+    if (!_screen.width)
+    {
+        gfx_get_screen_dimensions(&_screen);
+    }
+
     gfx_fill_rectangle(&_screen, 0, 0, GFX_COLOR_GRAY);
 
     gfx_dimensions bar = {_screen.width, 9};
@@ -51,179 +41,6 @@ _draw_background(void)
     gfx_fill_rectangle(&bar, 0, _screen.height - bar.height, GFX_COLOR_BLACK);
     gfx_draw_text("(C) 2021-2023", 1, 24);
     gfx_draw_text("https://github.com/thecatkitty/lavender/", 39, 24);
-}
-
-static void
-_draw_frame(int columns, int lines, const char *title, int title_length)
-{
-    gfx_dimensions window = {_glyph.width * (columns + 3),
-                             _glyph.height * (lines + 3)};
-
-    int left = (_screen.width - window.width) / 2;
-    int top = (_screen.height - window.height) / 16 * _glyph.height;
-    gfx_fill_rectangle(&window, left, top, GFX_COLOR_WHITE);
-    gfx_draw_rectangle(&window, left, top, GFX_COLOR_BLACK);
-    window.width += 2;
-    gfx_draw_rectangle(&window, left - 1, top, GFX_COLOR_BLACK);
-
-    gfx_dimensions title_line = {window.width - 1, 1};
-    gfx_draw_line(&title_line, left, top + 9, GFX_COLOR_BLACK);
-
-    gfx_dimensions stripe = {
-        (window.width - ((title_length + 2) * _glyph.width)) / 2 - 1, 1};
-    for (int i = 0; i < 4; i++)
-    {
-        int y = top + 1 + i * 2;
-        gfx_draw_line(&stripe, left + 1, y, GFX_COLOR_BLACK);
-        gfx_draw_line(&stripe, left + window.width - stripe.width - 4, y,
-                      GFX_COLOR_BLACK);
-    }
-
-#ifdef UTF8_NATIVE
-    const char *title_l = title;
-#else
-    char *title_l = alloca(strlen(title) + 1);
-    utf8_encode(title, title_l, gfx_wctob);
-#endif
-    gfx_draw_text(title_l, (_screen.width / _glyph.width - title_length) / 2,
-                  top / _glyph.height);
-}
-
-static int
-_strndcpy(char *dst, const char *src, size_t count, char delimiter)
-{
-    int i = 0;
-#ifdef UTF8_NATIVE
-    int chars = 0;
-#endif
-    while (src[i])
-    {
-        if (delimiter == src[i])
-        {
-            dst[i] = 0;
-            return i + 1;
-        }
-
-#ifdef UTF8_NATIVE
-        if (count == chars)
-#else
-        if (count == i)
-#endif
-        {
-            break;
-        }
-
-        dst[i] = src[i];
-#ifdef UTF8_NATIVE
-        if ((0x80 > (uint8_t)src[i]) || (0xBF < (uint8_t)src[i]))
-        {
-            ++chars;
-        }
-#endif
-
-        ++i;
-    }
-
-    dst[i] = 0;
-    return i;
-}
-
-static bool
-_draw_text(int columns, int lines, const char *text)
-{
-#ifdef UTF8_NATIVE
-    const char *text_l = text;
-#else
-    char *text_l = alloca(strlen(text) + 1);
-    utf8_encode(text, text_l, gfx_wctob);
-#endif
-
-    const char *fragment = text_l;
-#ifdef UTF8_NATIVE
-    char *line_buff = alloca(columns * 2 + 1);
-#else
-    char *line_buff = alloca(columns + 1);
-#endif
-
-    int left = (_screen.width / _glyph.width - columns) / 2;
-    int top = (_screen.height / _glyph.height - 3 - lines) / 2 + 2;
-
-    int line = 0;
-    while (*fragment && (lines > line))
-    {
-        fragment += _strndcpy(line_buff, fragment, columns, '\n');
-        if (!gfx_draw_text(line_buff, left, top + line))
-        {
-            return false;
-        }
-        line++;
-    }
-
-    return true;
-}
-
-static int
-_get_content_width(const char *text, int limit)
-{
-    int max = 0, current = 0;
-    while (*text)
-    {
-        int seq_length;
-        if (L'\n' == utf8_get_codepoint(text, &seq_length))
-        {
-            if (max < current)
-            {
-                max = current;
-            }
-
-            current = 0;
-        }
-
-        text += (0 == seq_length) ? 1 : seq_length;
-        current++;
-    }
-
-    if (max < current)
-    {
-        max = current;
-    }
-
-    if (limit && (limit < max))
-    {
-        return limit;
-    }
-
-    return max;
-}
-
-static int
-_get_content_height(const char *text)
-{
-    int count = 1;
-    while (*text)
-    {
-        if ('\n' == *text)
-        {
-            count++;
-        }
-        text++;
-    }
-    return count;
-}
-
-static void
-_get_content_size(
-    const char *title, const char *message, int *head, int *columns, int *lines)
-{
-    if (!_screen.width)
-    {
-        gfx_get_screen_dimensions(&_screen);
-        gfx_get_glyph_dimensions(&_glyph);
-    }
-
-    *head = _get_content_width(title, _screen.width / 10);
-    *columns = _get_content_width(message, _screen.width / 10);
-    *lines = _get_content_height(message);
 }
 
 static struct
@@ -286,109 +103,299 @@ dlg_alert(const char *title, const char *message)
     return true;
 }
 
-static void
-_draw_text_box(void)
+static struct
 {
-    if (_validator && !_validator(_buffer))
-    {
-        gfx_draw_rectangle(&_box, _box_left, _box_top, GFX_COLOR_GRAY);
-    }
-    else
-    {
-        gfx_draw_rectangle(&_box, _box_left, _box_top, GFX_COLOR_BLACK);
-    }
-
-    gfx_fill_rectangle(&_box, _box_left, _box_top, GFX_COLOR_WHITE);
-    gfx_draw_text(_buffer, _field_left, _field_top);
-}
+    LPWSTR        title;
+    LPWSTR        message;
+    HANDLE        thread;
+    char         *buffer;
+    int           size;
+    dlg_validator validator;
+    int           value;
+} _prompt_data;
 
 static int
 _handle_prompt(void)
 {
-    if (STATE_PROMPT_INVALID1 == _state)
+    if (DLG_INCOMPLETE != _prompt_data.value)
     {
-        if (pal_get_counter() > _blink_start + pal_get_ticks(63))
+        if (WAIT_TIMEOUT == WaitForSingleObject(_prompt_data.thread, 1))
         {
-            gfx_draw_rectangle(&_box, _box_left, _box_top, GFX_COLOR_GRAY);
-            _state = STATE_PROMPT_INVALID2;
+            return DLG_INCOMPLETE;
         }
 
-        return DLG_INCOMPLETE;
-    }
-
-    if (STATE_PROMPT_INVALID2 == _state)
-    {
-        if (pal_get_counter() > _blink_start + pal_get_ticks(126))
-        {
-            gfx_draw_rectangle(&_box, _box_left, _box_top, GFX_COLOR_BLACK);
-            _state = STATE_PROMPT_INVALID3;
-        }
-
-        return DLG_INCOMPLETE;
-    }
-
-    if (STATE_PROMPT_INVALID3 == _state)
-    {
-        if (pal_get_counter() > _blink_start + pal_get_ticks(189))
-        {
-            gfx_draw_rectangle(&_box, _box_left, _box_top, GFX_COLOR_GRAY);
-            _draw_text_box();
-            _state = STATE_PROMPT;
-        }
-
-        return DLG_INCOMPLETE;
-    }
-
-    uint16_t scancode = pal_get_keystroke();
-    if (0 == scancode)
-    {
-        return DLG_INCOMPLETE;
-    }
-
-    if (VK_ESCAPE == scancode)
-    {
+        CloseHandle(_prompt_data.thread);
+        _prompt_data.thread = NULL;
         _state = STATE_NONE;
-        return 0;
     }
 
-    if (VK_RETURN == scancode)
+    return _prompt_data.value;
+}
+
+#define ID_EDITBOX 150
+#define ID_TEXT    200
+
+static BOOL CALLBACK
+_dialog_proc(HWND dlg, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
     {
-        if (_validator && _validator(_buffer))
+    case WM_INITDIALOG: {
+        // Get window and dialog client and non-client rects
+        RECT wnd_rect;
+        GetWindowRect(windows_get_hwnd(), &wnd_rect);
+        LONG window_width = wnd_rect.right - wnd_rect.left;
+        LONG window_height = wnd_rect.bottom - wnd_rect.top;
+
+        RECT dlg_rect;
+        GetWindowRect(dlg, &dlg_rect);
+        LONG border_height = dlg_rect.bottom - dlg_rect.top;
+        LONG border_width = dlg_rect.right - dlg_rect.left;
+
+        RECT cl_rect;
+        GetClientRect(dlg, &cl_rect);
+        border_height -= cl_rect.bottom - cl_rect.top;
+        border_width -= cl_rect.right - cl_rect.left;
+
+        // Get text position
+        RECT text_rect;
+        GetWindowRect(GetDlgItem(dlg, ID_TEXT), &text_rect);
+        MapWindowPoints(HWND_DESKTOP, dlg, (LPPOINT)&text_rect, 2);
+        LONG padding_x = text_rect.left;
+        LONG padding_y = text_rect.top;
+        text_rect.right = text_rect.left + (window_width / 2) - padding_x;
+
+        // Get edit box position
+        RECT edit_rect;
+        GetClientRect(GetDlgItem(dlg, ID_EDITBOX), &edit_rect);
+
+        // Get button position
+        RECT button_rect;
+        GetClientRect(GetDlgItem(dlg, IDOK), &button_rect);
+
+        // Measure text
+        HDC   dc = GetDC(dlg);
+        HFONT prev_font =
+            SelectObject(dc, (HFONT)SendMessageW(dlg, WM_GETFONT, 0, 0));
+        DrawTextW(dc, (LPCWSTR)lparam, -1, &text_rect,
+                  DT_LEFT | DT_EXPANDTABS | DT_WORDBREAK | DT_CALCRECT |
+                      DT_NOPREFIX);
+        SelectObject(dc, prev_font);
+        ReleaseDC(dlg, dc);
+
+        // Position everything
+        LONG cl_height = padding_y;
+
+        LONG text_width = text_rect.right - text_rect.left;
+        LONG text_height = text_rect.bottom - text_rect.top;
+        cl_height += text_height + padding_y;
+
+        LONG edit_height = edit_rect.bottom - edit_rect.top;
+        LONG edit_width = max(text_width, edit_rect.right - edit_rect.left);
+        LONG edit_top = cl_height;
+        cl_height += edit_height + 2 * padding_y;
+
+        LONG cl_width = padding_x + edit_width + padding_x;
+
+        LONG button_height = button_rect.bottom - button_rect.top;
+        LONG button_width = button_rect.right - button_rect.left;
+        LONG button_top = cl_height;
+        LONG button_left = cl_width - button_width - padding_x;
+        cl_height += button_height + padding_y;
+
+        SetWindowPos(
+            dlg, 0,
+            wnd_rect.left + (window_width - cl_width - border_width) / 2,
+            wnd_rect.top + (window_height - cl_height - border_height) / 2,
+            cl_width + border_width, cl_height + border_height,
+            SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOREDRAW);
+
+        SetWindowPos(GetDlgItem(dlg, ID_TEXT), 0, padding_x, padding_y,
+                     text_width, text_height,
+                     SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOREDRAW);
+
+        SetWindowPos(GetDlgItem(dlg, ID_EDITBOX), 0, padding_x, edit_top,
+                     edit_width, edit_height,
+                     SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOREDRAW);
+        SendMessageW(GetDlgItem(dlg, ID_EDITBOX), EM_LIMITTEXT,
+                     _prompt_data.size, 0);
+
+        SetWindowPos(GetDlgItem(dlg, IDOK), 0, button_left, button_top,
+                     button_width, button_height,
+                     SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOREDRAW);
+
+        // Activate edit box
+        SetFocus(GetDlgItem(dlg, ID_EDITBOX));
+
+        return FALSE;
+    }
+
+    case WM_COMMAND:
+        if ((EN_CHANGE == HIWORD(wparam)) && (ID_EDITBOX == LOWORD(wparam)))
         {
-            _state = STATE_NONE;
-            return _cursor;
+            if (!_prompt_data.validator)
+            {
+                return TRUE;
+            }
+
+            HWND   edit_box = GetDlgItem(dlg, ID_EDITBOX);
+            int    length = GetWindowTextLengthW(edit_box);
+            LPWSTR text = (LPWSTR)alloca((length + 1) * sizeof(WCHAR));
+            GetWindowTextW(edit_box, text, length + 1);
+
+            length =
+                WideCharToMultiByte(CP_UTF8, 0, text, -1, NULL, 0, NULL, NULL);
+            LPSTR atext = (LPSTR)alloca(length);
+            WideCharToMultiByte(CP_UTF8, 0, text, -1, atext, length, NULL,
+                                NULL);
+            EnableWindow(GetDlgItem(dlg, IDOK), _prompt_data.validator(atext));
+            return TRUE;
         }
 
-        if (!_validator)
+        switch (LOWORD(wparam))
         {
-            _state = STATE_NONE;
-            return _cursor;
+        case IDOK: {
+            HWND   edit_box = GetDlgItem(dlg, ID_EDITBOX);
+            int    length = GetWindowTextLengthW(edit_box);
+            LPWSTR text = (LPWSTR)alloca((length + 1) * sizeof(WCHAR));
+            GetWindowTextW(edit_box, text, length + 1);
+            WideCharToMultiByte(CP_UTF8, 0, text, -1, _prompt_data.buffer,
+                                _prompt_data.size, NULL, NULL);
+            EndDialog(dlg, wparam);
+            _prompt_data.value = length;
+            return TRUE;
         }
 
-        gfx_draw_rectangle(&_box, _box_left, _box_top, GFX_COLOR_BLACK);
-        _blink_start = pal_get_counter();
-        _state = STATE_PROMPT_INVALID1;
-        return DLG_INCOMPLETE;
-    }
+        case IDCANCEL:
+            EndDialog(dlg, wparam);
+            _prompt_data.value = 0;
+            // Fall through
 
-    if ((VK_BACK == scancode) && (0 < _cursor))
+        default:
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+static DWORD WINAPI
+_prompt_routine(LPVOID lpparam)
+{
+    // Dialog box
+    LPDLGTEMPLATE dt = (LPDLGTEMPLATE)GlobalLock(_hgbl);
+    ZeroMemory(dt, 1024);
+    dt->style = WS_POPUP | WS_BORDER | WS_SYSMENU | DS_MODALFRAME | WS_CAPTION |
+                DS_SETFONT;
+    dt->cdit = 3;
+
+    // no menu
+    LPWORD menu = (LPWORD)(dt + 1);
+    menu[0] = 0;
+
+    // default class
+    LPWORD class = (LPWORD)(menu + 1);
+    class[0] = 0;
+
+    // caption
+    LPWSTR caption = (LPWSTR)(class + 1);
+    int    caption_length = wcslen(_prompt_data.title) + 1;
+    wcscpy(caption, _prompt_data.title);
+
+    // font size
+    LPWORD font_size = (LPWORD)(caption + caption_length);
+    *font_size = _nclm.lfMessageFont.lfHeight;
+
+    // font face
+    LPWSTR font_face = (LPWSTR)(font_size + 1);
+    wcscpy(font_face, _nclm.lfMessageFont.lfFaceName);
+
+    // Declaration for later
+    LPWORD creation_data;
+
+    // OK button
+    LPDLGITEMTEMPLATE ok_button = (LPDLGITEMTEMPLATE)align(
+        font_face + wcslen(font_face) + 1, sizeof(DWORD));
+    ok_button->cx = 50;
+    ok_button->cy = 14;
+    ok_button->id = IDOK;
+    ok_button->style = WS_CHILD | WS_VISIBLE | WS_TABSTOP | BS_DEFPUSHBUTTON |
+                       (_prompt_data.validator ? WS_DISABLED : 0);
+
+    // button class
+    class = (LPWORD)(ok_button + 1);
+    class[0] = 0xFFFF;
+    class[1] = 0x0080;
+
+    // caption
+    caption = (LPWSTR)(class + 2);
+    caption_length = MultiByteToWideChar(CP_UTF8, 0, "OK", -1, caption, 50);
+
+    // no creation data
+    creation_data = (LPWORD)(caption + caption_length);
+    *creation_data = 0;
+
+    // Edit box
+    LPDLGITEMTEMPLATE edit_box =
+        (LPDLGITEMTEMPLATE)align(creation_data + 1, sizeof(DWORD));
+    edit_box->cx = (_prompt_data.size + 2) * 4;
+    edit_box->cy = 14;
+    edit_box->id = ID_EDITBOX;
+    edit_box->style = WS_CHILD | WS_VISIBLE | WS_BORDER | WS_TABSTOP |
+                      ES_AUTOHSCROLL | ES_UPPERCASE;
+
+    // static class
+    class = (LPWORD)(edit_box + 1);
+    class[0] = 0xFFFF;
+    class[1] = 0x0081;
+
+    // caption
+    caption = (LPWSTR)(class + 2);
+    *caption = 0;
+    caption_length = 1;
+
+    // no creation data
+    creation_data = (LPWORD)(caption + caption_length);
+    *creation_data = 0;
+
+    // Static text control
+    LPDLGITEMTEMPLATE label =
+        (LPDLGITEMTEMPLATE)align(creation_data + 1, sizeof(DWORD));
+    label->x = 5;
+    label->y = 5;
+    label->id = ID_TEXT;
+    label->style = WS_CHILD | WS_VISIBLE | SS_LEFT;
+
+    // static class
+    class = (LPWORD)(label + 1);
+    class[0] = 0xFFFF;
+    class[1] = 0x0082;
+
+    // caption
+    caption = (LPWSTR)(class + 2);
+    caption_length = wcslen(_prompt_data.message) + 1;
+    wcscpy(caption, _prompt_data.message);
+
+    // no creation data
+    creation_data = (LPWORD)(caption + caption_length);
+    *creation_data = 0;
+
+    GlobalUnlock(_hgbl);
+    DialogBoxIndirectParamW(GetModuleHandleW(NULL), (LPDLGTEMPLATE)_hgbl,
+                            windows_get_hwnd(), (DLGPROC)_dialog_proc,
+                            (LPARAM)caption);
+
+    free(_prompt_data.title);
+    free(_prompt_data.message);
+    return 0;
+}
+
+static void
+_free_hgbl(void)
+{
+    if (_hgbl)
     {
-        _cursor--;
-        _buffer[_cursor] = 0;
-        _draw_text_box();
+        GlobalFree(_hgbl);
     }
-
-    if (((' ' == scancode) || (VK_OEM_MINUS == scancode) ||
-         ((VK_DELETE < scancode) && (VK_F1 > scancode))) &&
-        (_cursor < _size))
-    {
-        _buffer[_cursor] = (VK_OEM_MINUS == scancode) ? '-' : (scancode & 0xFF);
-        _cursor++;
-        _buffer[_cursor] = 0;
-        _draw_text_box();
-    }
-
-    return DLG_INCOMPLETE;
 }
 
 bool
@@ -403,31 +410,46 @@ dlg_prompt(const char   *title,
         return false;
     }
 
-    int title_length, columns, lines;
-    _get_content_size(title, message, &title_length, &columns, &lines);
-    lines += 2;
+    if (0 == _nclm.cbSize)
+    {
+        _nclm.cbSize = sizeof(_nclm);
+        if (!SystemParametersInfoW(SPI_GETNONCLIENTMETRICS, sizeof(_nclm),
+                                   &_nclm, 0))
+        {
+            _nclm.cbSize = 0;
+            return false;
+        }
+    }
 
-    int field_width = (columns > size) ? size : columns;
-    _field_left = (_screen.width / _glyph.width - field_width) / 2;
-    _field_top = (_screen.height / _glyph.height - 3 - lines) / 2 + 1 + lines;
+    if (!_hgbl)
+    {
+        _hgbl = GlobalAlloc(0, 1024);
+        if (!_hgbl)
+        {
+            return false;
+        }
 
-    _box.width = field_width * _glyph.width + 2;
-    _box.height = 10;
-    _box_top = _field_top * _glyph.height - 1;
-    _box_left = _field_left * _glyph.width - 1;
+        atexit(_free_hgbl);
+    }
+
+    int title_length = MultiByteToWideChar(CP_UTF8, 0, title, -1, NULL, 0);
+    _prompt_data.title = (LPWSTR)malloc(title_length * sizeof(WCHAR));
+    MultiByteToWideChar(CP_UTF8, 0, title, -1, _prompt_data.title,
+                        title_length);
+
+    int message_length = MultiByteToWideChar(CP_UTF8, 0, message, -1, NULL, 0);
+    _prompt_data.message = (LPWSTR)malloc(message_length * sizeof(WCHAR));
+    MultiByteToWideChar(CP_UTF8, 0, message, -1, _prompt_data.message,
+                        message_length);
+
+    _prompt_data.buffer = buffer;
+    _prompt_data.size = size;
+    _prompt_data.validator = validator;
+
+    _prompt_data.thread = CreateThread(NULL, 0, _prompt_routine, NULL, 0, NULL);
+    _prompt_data.value = DLG_INCOMPLETE;
 
     _draw_background();
-    _draw_frame(columns, lines, title, title_length);
-    _draw_text(columns, lines, message);
-
-    _state = STATE_PROMPT;
-    _buffer = buffer;
-    _size = size;
-    _validator = validator;
-    _cursor = 0;
-
-    _buffer[0] = 0;
-    _draw_text_box();
 
     _state = STATE_PROMPT;
     return true;
