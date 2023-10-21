ad92d187639d536a17b15c29b42b5214764c9e45
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-10-21 21:37:55 +0200

    snd: add basic 6-voice playback with OPL2

---
+++ src/snd/opl2.h
@@ -0,0 +1,26 @@
+#include <base.h>
+
+#pragma pack(push, 1)
+typedef struct
+{
+    uint8_t car_wave_select : 2;
+    uint8_t mod_wave_select : 2;
+    uint8_t feedback_conn_reg : 4;
+
+    uint8_t car_level;
+    uint8_t car_amvib_reg;
+    uint8_t car_decay : 4;
+    uint8_t car_attack : 4;
+    uint8_t car_release : 4;
+    uint8_t car_sustain : 4;
+
+    uint8_t mod_level;
+    uint8_t mod_amvib_reg;
+    uint8_t mod_decay : 4;
+    uint8_t mod_attack : 4;
+    uint8_t mod_release : 4;
+    uint8_t mod_sustain : 4;
+} opl2_patch;
+#pragma pack(pop)
+
+extern const opl2_patch __snd_gm_opl2[128];
