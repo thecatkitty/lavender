937b3b63c6d05823058cb9a06b2a17dbeb583c60
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-01-16 21:03:21 +0100

    repo: add CMake configuration for Linux

---
+++ cmake/version.cmake
@@ -0,0 +1,18 @@
+execute_process(
+    COMMAND git describe --abbrev=0
+    OUTPUT_VARIABLE GIT_TAG
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(
+    COMMAND git rev-list ${GIT_TAG}..HEAD --count
+    OUTPUT_VARIABLE GIT_COMMITS
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+string(REPLACE "." "," VER_FILEVERSION "${GIT_TAG}.${GIT_COMMITS}")
+if(GIT_COMMITS STREQUAL "0")
+    set(VER_FILEVERSION_STR "${GIT_TAG}")
+else()
+    set(VER_FILEVERSION_STR "${GIT_TAG}-${GIT_COMMITS}")
+endif()
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/version.h.in version.h @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/version.txt.in version.txt @ONLY)
