101b58e9c5756be566074b7825a41a4fbef27e78
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-01-21 23:35:14 +0100

    repo: remove Makefiles

--- src/dlg/module.mk
+++
@@ -1,5 +0,0 @@
-ifeq ($(LAV_TARGET),windows)
-CCSOURCES := $(CCSOURCES) $(DIR)/windows.c
-else
-CCSOURCES := $(CCSOURCES) $(DIR)/fullscrn.c
-endif
--- src/fmt/module.mk
+++
@@ -1,8 +0,0 @@
-CCSOURCES := $(CCSOURCES) \
-	$(DIR)/utf8.c \
-	$(DIR)/exe.c \
-	$(DIR)/zip.c \
-	$(DIR)/pbm.c \
-	$(DIR)/bmp.c \
-	$(DIR)/iff.c \
-	$(DIR)/midi.c
