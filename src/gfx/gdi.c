e634ef2b7a99944c35e59da5fdf19fa1d25f7a0b
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-09-14 00:11:41 +0200

    gfx: gdi: add drawing rectangles

--- src/gfx/gdi.c
+++ src/gfx/gdi.c
@@ -7,6 +7,23 @@ static HFONT _font = NULL;
 static HWND  _wnd = NULL;
 static SIZE  _glyph;
 
+static const COLORREF COLORS[] = {[GFX_COLOR_BLACK] = RGB(0, 0, 0),
+                                  [GFX_COLOR_NAVY] = RGB(0, 0, 128),
+                                  [GFX_COLOR_GREEN] = RGB(0, 128, 0),
+                                  [GFX_COLOR_TEAL] = RGB(0, 128, 128),
+                                  [GFX_COLOR_MAROON] = RGB(128, 0, 0),
+                                  [GFX_COLOR_PURPLE] = RGB(128, 0, 128),
+                                  [GFX_COLOR_OLIVE] = RGB(128, 128, 0),
+                                  [GFX_COLOR_SILVER] = RGB(192, 192, 192),
+                                  [GFX_COLOR_GRAY] = RGB(128, 128, 128),
+                                  [GFX_COLOR_BLUE] = RGB(0, 0, 255),
+                                  [GFX_COLOR_LIME] = RGB(0, 255, 0),
+                                  [GFX_COLOR_CYAN] = RGB(0, 255, 255),
+                                  [GFX_COLOR_RED] = RGB(255, 0, 0),
+                                  [GFX_COLOR_FUCHSIA] = RGB(255, 0, 255),
+                                  [GFX_COLOR_YELLOW] = RGB(255, 255, 0),
+                                  [GFX_COLOR_WHITE] = RGB(255, 255, 255)};
+
 bool
 gfx_initialize(void)
 {
@@ -62,6 +79,18 @@ gfx_get_color_depth(void)
     return 24;
 }
 
+static void
+_to_wrect(const gfx_rect *rect, RECT *wrect)
+{
+    RECT crect;
+    GetClientRect(_wnd, &crect);
+
+    wrect->left = crect.left + rect->left;
+    wrect->top = crect.top + rect->top;
+    wrect->right = wrect->left + rect->width;
+    wrect->bottom = wrect->top + rect->height;
+}
+
 bool
 gfx_draw_bitmap(gfx_bitmap *bm, int x, int y)
 {
@@ -77,12 +106,30 @@ gfx_draw_line(gfx_rect *rect, gfx_color color)
 bool
 gfx_draw_rectangle(gfx_rect *rect, gfx_color color)
 {
+    RECT wrect;
+    _to_wrect(rect, &wrect);
+    wrect.left--;
+    wrect.top--;
+    wrect.right++;
+    wrect.bottom++;
+
+    HDC dc = GetDC(_wnd);
+    SetDCBrushColor(dc, COLORS[color]);
+    FrameRect(dc, &wrect, GetStockObject(DC_BRUSH));
+    ReleaseDC(_wnd, dc);
     return true;
 }
 
 bool
 gfx_fill_rectangle(gfx_rect *rect, gfx_color color)
 {
+    RECT wrect;
+    _to_wrect(rect, &wrect);
+
+    HDC dc = GetDC(_wnd);
+    SetDCBrushColor(dc, COLORS[color]);
+    FillRect(dc, &wrect, GetStockObject(DC_BRUSH));
+    ReleaseDC(_wnd, dc);
     return true;
 }
 
