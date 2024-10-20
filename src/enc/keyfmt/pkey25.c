de1c89d8e50ce368ffa3ecbd7591c522f911f238
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-10-20 14:47:30 +0200

    enc: move PKEY25 format handling to enc

--- src/enc/keyfmt/pkey25.c
+++ src/enc/keyfmt/pkey25.c
@@ -1,5 +1,13 @@
 #include <enc.h>
 
+// PKEY25XOR12 definitions
+#define PKEY25XOR12_BASE         24
+#define PKEY25XOR12_UDATA_LENGTH 13
+#define PKEY25XOR12_EKEY_LENGTH  12
+#define PKEY25XOR12_LENGTH       (PKEY25XOR12_UDATA_LENGTH + PKEY25XOR12_EKEY_LENGTH)
+
+static const char PKEY25XOR12_ALPHABET[] = "2346789BCDFGHJKMPQRTVWXY";
+
 static uint64_t
 _aatoull(const char *str, size_t length, const char *alphabet, size_t base)
 {
@@ -21,16 +29,32 @@ _aatoull(const char *str, size_t length, const char *alphabet, size_t base)
 uint64_t
 __enc_pkey25xor12_decode(const void *src)
 {
-    const char *left_part = (const char *)src;
-    const char *right_part = (const char *)src + PKEY25XOR12_UDATA_LENGTH;
+    const char *str = (const char *)src;
 
-    uint64_t udata =
+    // Remove hyphens
+    char  pkey[PKEY25XOR12_LENGTH];
+    char *ppkey = pkey;
+    while (ppkey < pkey + PKEY25XOR12_LENGTH)
+    {
+        if ('-' != *str)
+        {
+            *ppkey = *str;
+            ppkey++;
+        }
+        str++;
+    }
+
+    // Convert to integer parts
+    const char *left_part = pkey;
+    const char *right_part = pkey + PKEY25XOR12_UDATA_LENGTH;
+    uint64_t    udata =
         _aatoull(left_part, PKEY25XOR12_UDATA_LENGTH, PKEY25XOR12_ALPHABET, 24);
     uint64_t ekey =
         _aatoull(right_part, PKEY25XOR12_EKEY_LENGTH, PKEY25XOR12_ALPHABET, 24);
-    uint64_t key_56 = ekey ^ udata;
 
-    uint8_t key[sizeof(uint64_t)];
+    // Retrieve the 64-bit key
+    uint64_t key_56 = ekey ^ udata;
+    uint8_t  key[sizeof(uint64_t)];
     for (size_t b = 0; b < lengthof(key); b++)
     {
         key[b] = ((uint8_t)key_56 & 0x7F) << 1;
@@ -42,4 +66,27 @@ __enc_pkey25xor12_decode(const void *src)
     }
 
     return *(uint64_t *)key;
+}
+
+bool
+__enc_pkey25xor12_validate_format(const char *key)
+{
+    size_t i;
+    for (i = 0; i < PKEY25XOR12_LENGTH + 4; i++)
+    {
+        if (5 == (i % 6))
+        {
+            if ('-' != key[i])
+            {
+                return false;
+            }
+        }
+        else if ((0 == key[i]) ||
+                 (NULL == strchr(PKEY25XOR12_ALPHABET, key[i])))
+        {
+            return false;
+        }
+    }
+
+    return PKEY25XOR12_LENGTH + 4 == i;
 }
