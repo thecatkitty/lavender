0be38df7ac55fba19ee6a43ef413793b450876d5
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-10-24 22:32:04 +0200

    enc: move key acquisition to enc

--- inc/enc.h
+++ inc/enc.h
@@ -14,9 +14,18 @@ typedef enum
 
 typedef enum
 {
+    ENC_KEYSM_RAW,         // not transformed
     ENC_KEYSM_LE32B6D,     // 32 local bits, 6 external decimals
     ENC_KEYSM_PKEY25XOR12, // 25-character product key, XORed 12 characters
 } enc_keysm;
+
+typedef enum
+{
+    ENC_PROVIDER_CALLER, // provided by the caller
+    ENC_PROVIDER_PROMPT, // entered by the user
+    ENC_PROVIDER_SPLIT,  // part from the caller, completed by the user
+    ENC_PROVIDER_DISKID, // part from the diskette, completed by the user
+} enc_provider;
 
 typedef struct
 {
@@ -27,6 +36,39 @@ typedef struct
     const void    *_impl;
     void          *_context;
 } enc_stream;
+
+typedef struct
+{
+    uint32_t local_part;
+    uint32_t passcode;
+} enc_split_data;
+
+typedef struct
+{
+    enc_split_data split;
+    char           dsn[16];
+} enc_diskid_data;
+
+typedef struct
+{
+    enc_cipher  cipher;
+    int         provider;
+    const void *parameter;
+    uint8_t    *content;
+    int         size;
+    uint32_t    crc32;
+
+    int        state;
+    enc_stream stream;
+    uquad      key;
+    char       buffer[24];
+    union {
+        enc_split_data  split;
+        enc_diskid_data diskid;
+    } data;
+} enc_context;
+
+#define ENC_KEYSRC(p, sm) (((sm) << 8) | (p))
 
 extern bool
 enc_prepare(enc_stream    *stream,
@@ -53,6 +95,18 @@ enc_decode_key(const void *src, enc_keysm sm);
 
 extern bool
 enc_validate_key_format(const char *key, enc_keysm sm);
+
+extern int
+enc_access_content(enc_context *enc,
+                   enc_cipher   cipher,
+                   int          provider,
+                   const void  *parameter,
+                   uint8_t     *content,
+                   size_t       length,
+                   uint32_t     crc32);
+
+extern int
+enc_handle(enc_context *enc);
 #endif // CONFIG_ENCRYPTED_CONTENT
 
 #endif // _ENC_H_
