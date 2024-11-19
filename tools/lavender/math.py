50793c10d13d150f7e925224e6661fd286aeb7fb
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-11-19 21:51:36 +0100

    tools: add remote key basic routines

--- tools/lavender/math.py
+++ tools/lavender/math.py
@@ -1,3 +1,12 @@
+def complement(n: int, base: int = 10) -> int:
+    sum = 0
+    while n:
+        sum += n % base
+        n //= base
+
+    return (base - (sum % base)) % base
+
+
 def parity(n: int) -> int:
     result = 0
     while n:
