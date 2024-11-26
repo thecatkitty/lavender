72d730345f2793aa70f1ac312a49ae7f656082b6
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-11-26 21:56:56 +0100

    enc: ui: add complex pages

--- src/enc/ui/encui.h
+++ src/enc/ui/encui.h
@@ -18,6 +18,21 @@ enum
     ENCUIM_NEXT,
 };
 
+enum
+{
+    ENCUIFT_SEPARATOR,
+    ENCUIFT_LABEL,
+    ENCUIFT_TEXTBOX,
+};
+
+#define ENCUIFF_STATIC  (0 << 0)
+#define ENCUIFF_DYNAMIC (1 << 0)
+
+#define ENCUIFF_LEFT   (0 << 1)
+#define ENCUIFF_CENTER (1 << 1)
+#define ENCUIFF_RIGHT  (2 << 1)
+#define ENCUIFF_ALIGN  (3 << 1)
+
 typedef int(encui_page_proc)(int msg, void *param, void *data);
 
 typedef struct
@@ -29,13 +44,27 @@ typedef struct
 
 typedef struct
 {
+    int      type;
+    int      flags;
+    intptr_t data;
+} encui_field;
+
+typedef struct
+{
+    int          length;
+    encui_field *fields;
+} encui_complex_page;
+
+typedef struct
+{
     int              title;
     int              message;
     encui_page_proc *proc;
     void            *data;
 
     union {
-        encui_prompt_page prompt;
+        encui_prompt_page  prompt;
+        encui_complex_page cpx;
     };
 } encui_page;
 
@@ -53,5 +82,8 @@ encui_set_page(int id);
 
 extern int
 encui_handle(void);
+
+extern encui_prompt_page *
+encui_find_prompt(encui_page *page);
 
 #endif // _ENCUI_H_
