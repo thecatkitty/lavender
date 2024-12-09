1d7ee57091f6e7635c7e2722600b1f81f3cf2fc1
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-12-09 21:05:50 +0100

    enc: ui: extract textbox code

--- src/enc/ui/direct/direct.h
+++ src/enc/ui/direct/direct.h
@@ -19,4 +19,34 @@ encui_direct_get_checkbox_area(void);
 void
 encui_direct_click_checkbox(encui_field *field);
 
+void
+encui_direct_create_textbox(encui_prompt_page *prompt,
+                            encui_page        *page,
+                            int               *cy);
+
+void
+encui_direct_draw_textbox(encui_prompt_page *prompt, encui_page *page);
+
+bool
+encui_direct_animate_textbox(encui_prompt_page *prompt,
+                             encui_page        *page,
+                             bool               valid);
+
+void
+encui_direct_set_textbox_error(char *message);
+
+const gfx_rect *
+encui_direct_get_textbox_area(void);
+
+void
+encui_direct_click_textbox(encui_prompt_page *prompt,
+                           encui_page        *page,
+                           uint16_t           x,
+                           uint16_t           y);
+
+void
+encui_direct_key_textbox(encui_prompt_page *prompt,
+                         encui_page        *page,
+                         uint16_t           scancode);
+
 #endif
