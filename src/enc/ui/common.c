1c7222c78f475a0a45227443375a573460951698
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-12-09 06:59:47 +0000

    enc: ui: extract common lookup code

--- src/enc/ui/common.c
+++ src/enc/ui/common.c
@@ -1,39 +1,29 @@
 #include "encui.h"
 
-encui_field *
-encui_find_checkbox(encui_page *page)
+static encui_field *
+_find_field_by_type(encui_page *page, int type)
 {
-    encui_field *checkbox = NULL;
-    int          i;
-
+    int i;
     for (i = 0; i < page->length; i++)
     {
-        encui_field *field = page->fields + i;
-        if (ENCUIFT_CHECKBOX == field->type)
+        if (type == page->fields[i].type)
         {
-            return field;
-            break;
+            return page->fields + i;
         }
     }
 
-    return checkbox;
+    return NULL;
+}
+
+encui_field *
+encui_find_checkbox(encui_page *page)
+{
+    return _find_field_by_type(page, ENCUIFT_CHECKBOX);
 }
 
 encui_prompt_page *
 encui_find_prompt(encui_page *page)
 {
-    encui_prompt_page *prompt = NULL;
-    int                i;
-
-    for (i = 0; i < page->length; i++)
-    {
-        encui_field *field = page->fields + i;
-        if (ENCUIFT_TEXTBOX == field->type)
-        {
-            prompt = (encui_prompt_page *)field->data;
-            break;
-        }
-    }
-
-    return prompt;
+    encui_field *field = _find_field_by_type(page, ENCUIFT_TEXTBOX);
+    return field ? (encui_prompt_page *)field->data : NULL;
 }
