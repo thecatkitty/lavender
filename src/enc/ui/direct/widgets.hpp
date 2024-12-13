783429143b330bd7147728d1ac848172e4ae5dcb
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-12-13 11:08:47 +0000

    enc: ui: add checkbox widget

--- src/enc/ui/direct/widgets.hpp
+++ src/enc/ui/direct/widgets.hpp
@@ -76,6 +76,23 @@ struct widget
     widget           *parent_;
 };
 
+struct checkbox : widget
+{
+    checkbox(const encui_page &page, encui_field &field);
+
+    virtual void
+    draw() override;
+
+    virtual int
+    click(int x, int y) override;
+
+  private:
+    void
+    mark(bool checked);
+
+    gfx_rect box_;
+};
+
 struct label : widget
 {
     label(const encui_page &page, encui_field &field) : widget{page, field}
