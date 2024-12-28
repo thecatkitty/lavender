ad553644d8ff28e10628a8423e271dcf1ae78a0e
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-12-28 19:26:40 +0100

    enc: ui: add bitmap widget

---
+++ src/enc/ui/direct/bitmap.cpp
@@ -0,0 +1,38 @@
+#include "widgets.hpp"
+
+using namespace ui;
+
+bitmap::bitmap(encui_field &field) : widget{field}
+{
+    gfx_dimensions glyph;
+    gfx_get_glyph_dimensions(&glyph);
+
+    auto &bm = *reinterpret_cast<gfx_bitmap *>(field_.data);
+    rect_.width = (bm.width + glyph.width - 1) / glyph.width;
+    rect_.height = (bm.height + glyph.height - 1) / glyph.height;
+}
+
+void
+bitmap::draw()
+{
+    gfx_dimensions glyph;
+    gfx_get_glyph_dimensions(&glyph);
+
+    auto &bm = *reinterpret_cast<gfx_bitmap *>(field_.data);
+    auto  pos = get_position();
+
+    auto x = pos.left;
+    if (ENCUIFF_CENTER == (ENCUIFF_ALIGN & field_.flags))
+    {
+        x += (TEXT_WIDTH - rect_.width) / 2;
+    }
+    else if (ENCUIFF_RIGHT == (ENCUIFF_ALIGN & field_.flags))
+    {
+        x += TEXT_WIDTH - rect_.width;
+    }
+    x *= glyph.width;
+
+    auto y = pos.top * glyph.height;
+
+    gfx_draw_bitmap(&bm, x, y);
+}
