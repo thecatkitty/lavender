5ff90fd8bbe6f1726b23114c792abc10c4751ae5
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-12-29 21:40:31 +0100

    enc: add remote provider method selection

--- src/enc/provider/remote.c
+++ src/enc/provider/remote.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,10 +17,11 @@
 enum
 {
     PAGE_PKEY,
+    PAGE_METHOD,
     PAGE_RCODE,
 };
 
-static encui_page _pages[3];
+static encui_page _pages[4];
 static uint8_t    _rbytes[18];
 static char       _rcode[64];
 static uint8_t    _cbytes[14];
@@ -57,27 +59,33 @@ _compl10(unsigned long n)
 }
 
 static void
-_get_rcode(uint32_t cid, const uint8_t *uid, uint32_t stamp)
+_stamp_rbytes(void)
 {
-    int i, offset = 0;
-
+    uint32_t stamp = time(NULL);
     memcpy(_rbytes + 0, &stamp, 4);
-    memcpy(_rbytes + 4, &cid, 4);
-    memcpy(_rbytes + 8, uid, 10);
+}
 
+static void
+_encode_rbytes(uint8_t* out)
+{
+    int i;
+
+    memcpy(out, _rbytes, sizeof(_rbytes));
     for (i = 1; i < lengthof(_rbytes); i++)
     {
-        _rbytes[i] ^= _rbytes[i - 1];
+        out[i] ^= out[i - 1];
     }
-
-    for (i = 0; i < lengthof(_rbytes); i += sizeof(uint16_t))
-    {
-        unsigned long word = *(uint16_t *)(_rbytes + i);
-        unsigned long compl10 = _compl10(word);
-        offset += sprintf(_rcode + offset, "%lu%05lu-", compl10, word);
-    }
-    _rcode[offset - 1] = 0;
 }
+
+// ----- Access code input
+
+static encui_textbox_data _acode_textbox = {NULL};
+
+static encui_field _acode_fields[] = {
+    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_ENTERPKEY_DESC},
+    {ENCUIFT_SEPARATOR, 0, 1},
+    {ENCUIFT_TEXTBOX, 0, (intptr_t)&_acode_textbox},
+};
 
 static int
 _acode_page_proc(int msg, void *param, void *data)
@@ -99,8 +107,7 @@ _acode_page_proc(int msg, void *param, void *data)
         uint64_t     parts[2] = {0, 0};
         uint32_t     cid;
         uint64_t     uid_left, uid_right;
-        uint8_t      uid[10];
-        time_t       stamp;
+        uint8_t     *uid = _rbytes + 8;
 
         enc_decode_key(enc->buffer, parts, ENC_KEYSM_PKEY25RAW);
         if (_parity(parts[0]) || _parity(parts[1]))
@@ -123,6 +130,7 @@ _acode_page_proc(int msg, void *param, void *data)
         {
             return IDS_WRONGCID;
         }
+        memcpy(_rbytes + 4, &cid, 4);
 
         // 80-bit user ID in big endian
         uid[0] = (uid_left >> 24) & 0xFF;
@@ -136,10 +144,6 @@ _acode_page_proc(int msg, void *param, void *data)
         uid[8] = (uid_right >> 8) & 0xFF;
         uid[9] = (uid_right >> 0) & 0xFF;
 
-        stamp = time(NULL);
-        _get_rcode(cid, uid, stamp);
-        _pages[PAGE_RCODE].fields[3].data = (intptr_t)_rcode;
-
         return 0;
     }
     }
@@ -147,11 +151,84 @@ _acode_page_proc(int msg, void *param, void *data)
     return -ENOSYS;
 }
 
+// ----- Method selection
+
+static encui_field _method_fields[] = {
+    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_METHOD_DESC},
+    {ENCUIFT_SEPARATOR, 0, 1},
+    {ENCUIFT_OPTION, ENCUIFF_STATIC | ENCUIFF_CHECKED, IDS_METHOD_RCODE},
+};
+
+static int
+_method_page_proc(int msg, void *param, void *data)
+{
+    switch (msg)
+    {
+    case ENCUIM_NEXT: {
+        encui_field *it = _pages[PAGE_METHOD].fields;
+        encui_field *end = it + _pages[PAGE_METHOD].length;
+
+        while ((it < end) && (0 == (ENCUIFF_CHECKED & it->flags)))
+        {
+            it++;
+        }
+        assert(it < end);
+
+        _stamp_rbytes();
+
+        if (IDS_METHOD_RCODE == it->data)
+        {
+            encui_set_page(PAGE_RCODE);
+            return -EINTR;
+        }
+    }
+    }
+
+    return -ENOSYS;
+}
+
+// ----- Request code with manual delivery
+
+static encui_textbox_data _ccode_textbox = {_ccode, sizeof(_ccode), 0};
+
+static encui_field _rcode_fields[] = {
+    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_UNLOCK_DESC},
+    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_RCODE_DESC},
+    {ENCUIFT_SEPARATOR, 0, 1},
+    {ENCUIFT_LABEL, ENCUIFF_DYNAMIC | ENCUIFF_CENTER, ID_RCODE},
+    {ENCUIFT_SEPARATOR, 0, 2},
+    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_CCODE_DESC},
+    {ENCUIFT_TEXTBOX, 0, (intptr_t)&_ccode_textbox},
+    {ENCUIFT_CHECKBOX, ENCUIFF_STATIC, IDS_STOREKEY},
+};
+
+static void
+_get_rcode(void)
+{
+    uint8_t rbytes[lengthof(_rbytes)];
+    int i, offset = 0;
+
+    _encode_rbytes(rbytes);
+    for (i = 0; i < lengthof(rbytes); i += sizeof(uint16_t))
+    {
+        unsigned long word = *(uint16_t *)(rbytes + i);
+        unsigned long compl10 = _compl10(word);
+        offset += sprintf(_rcode + offset, "%lu%05lu-", compl10, word);
+    }
+    _rcode[offset - 1] = 0;
+}
+
 static int
 _ccode_page_proc(int msg, void *param, void *data)
 {
     switch (msg)
     {
+    case ENCUIM_INIT: {
+        _get_rcode();
+        _pages[PAGE_RCODE].fields[3].data = (intptr_t)_rcode;
+        return 0;
+    }
+
     case ENCUIM_CHECK: {
         int i;
 
@@ -188,6 +265,7 @@ _ccode_page_proc(int msg, void *param, void *data)
         int         i;
         const char *ccode = (const char *)param;
         uint16_t   *cwords = (uint16_t *)_cbytes;
+        uint8_t     rbytes[lengthof(_rbytes)];
 
         for (i = 0; i < lengthof(_cbytes) / 2; i++)
         {
@@ -210,9 +288,10 @@ _ccode_page_proc(int msg, void *param, void *data)
             cwords[i] = (uint16_t)(group % 100000);
         }
 
+        _encode_rbytes(rbytes);
         for (i = 0; i < lengthof(_cbytes); i++)
         {
-            _cbytes[i] ^= _rbytes[i] ^ _rbytes[i + 2] ^ _rbytes[i + 4];
+            _cbytes[i] ^= rbytes[i] ^ rbytes[i + 2] ^ rbytes[i + 4];
         }
 
         return __enc_decrypt_content((enc_context *)data);
@@ -222,29 +301,9 @@ _ccode_page_proc(int msg, void *param, void *data)
     return -ENOSYS;
 }
 
-static encui_textbox_data _acode_textbox = {NULL};
-
-static encui_field _acode_fields[] = {
-    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_ENTERPKEY_DESC},
-    {ENCUIFT_SEPARATOR, 0, 1},
-    {ENCUIFT_TEXTBOX, 0, (intptr_t)&_acode_textbox},
-};
-
-static encui_textbox_data _ccode_textbox = {_ccode, sizeof(_ccode), 0};
-
-static encui_field _rcode_fields[] = {
-    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_UNLOCK_DESC},
-    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_RCODE_DESC},
-    {ENCUIFT_SEPARATOR, 0, 1},
-    {ENCUIFT_LABEL, ENCUIFF_DYNAMIC | ENCUIFF_CENTER, ID_RCODE},
-    {ENCUIFT_SEPARATOR, 0, 2},
-    {ENCUIFT_LABEL, ENCUIFF_STATIC, IDS_CCODE_DESC},
-    {ENCUIFT_TEXTBOX, 0, (intptr_t)&_ccode_textbox},
-    {ENCUIFT_CHECKBOX, ENCUIFF_STATIC, IDS_STOREKEY},
-};
-
 static encui_page _pages[] = {
     {IDS_ENTERPKEY, _acode_page_proc}, // PAGE_PKEY
+    {IDS_METHOD, _method_page_proc},   // PAGE_METHOD
     {IDS_UNLOCK, _ccode_page_proc},    // PAGE_RCODE
     {0}                                //
 };
@@ -283,6 +342,10 @@ __enc_remote_proc(int msg, enc_context *enc)
         _acode_textbox.buffer = enc->buffer;
         _acode_textbox.capacity = 5 * 5 + 4;
         _acode_textbox.length = 0;
+
+        _pages[PAGE_METHOD].data = enc;
+        _pages[PAGE_METHOD].length = lengthof(_method_fields);
+        _pages[PAGE_METHOD].fields = _method_fields;
 
         _pages[PAGE_RCODE].data = enc;
         _pages[PAGE_RCODE].length = lengthof(_rcode_fields);
