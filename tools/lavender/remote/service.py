719b89cf0c3d5839b011fcb1152d4f5e3f34df1d
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-01-06 21:26:01 +0100

    tools: add basic online verification service

--- tools/lavender/remote/service.py
+++ tools/lavender/remote/service.py
@@ -10,6 +10,11 @@ class AccessError(Exception):
         super().__init__(*args)
 
 
+class RequestError(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
 class LavenderService(object):
     def __init__(self, directory: UserDirectory, key: int):
         self._directory = directory
@@ -21,12 +26,12 @@ class LavenderService(object):
         now = int(time.time()) & 0xFFFFFFFF
 
         if stamp - now > 2 * 60:
-            raise AccessError("The request is from the future!")
+            raise RequestError("The request is from the future!")
 
         if now - stamp > 15 * 60:
-            raise AccessError("The request is stale!")
+            raise RequestError("The request is stale!")
 
         if not self._directory.has_access(uid, cid):
-            raise AccessError("User not authorized!")
+            raise AccessError("The user is not authorized!")
 
         return to_response(request, self._key)
