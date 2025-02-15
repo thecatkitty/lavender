73b403031409fed33e0d66b737122fd43550be6f
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-02-15 14:23:04 +0100

    ard: add the AutoRun Dependency Checker
    Load configuration and check the CPU compatibility.

---
+++ src/ard/resource.h
@@ -0,0 +1,20 @@
+#include <winnt.rh>
+
+#define IDS_DEFNAME      0x40
+#define IDS_DEFRUN       0x41
+#define IDS_DEFCPU       0x48
+
+#define IDS_OLDCPU       0x60
+
+#define IDS_CPUBASE      0x70
+#define IDS_CPUI486      0x71
+#define IDS_CPUP5        0x72
+#define IDS_CPUP6        0x73
+#define IDS_CPUSSE       0x74
+#define IDS_CPUSSE2      0x75
+#define IDS_CPUX64       0x76
+
+#define IDS_ECONFIG      0x101
+#define IDS_EEXECNORES   0x102
+#define IDS_EEXECBADF    0x103
+#define IDS_EEXECNOF     0x104
