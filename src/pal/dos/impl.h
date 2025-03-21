2f243818a8c10f7e26ae27a951c20b8f6dfecf06
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-03-21 07:26:21 +0000

    pal: dos: reuse cached assets

--- src/pal/dos/impl.h
+++ src/pal/dos/impl.h
@@ -7,4 +7,10 @@ extern gfx_glyph_data dos_font;
 extern gfx_dimensions dos_cell;
 extern bool           dos_mouse;
 
+extern void
+dos_initialize_cache(void);
+
+extern void
+dos_cleanup_cache(void);
+
 #endif // _PAL_DOS_IMPL_H_
