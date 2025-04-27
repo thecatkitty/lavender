5f816b2a96e00cd87485aa6427bb48f654515f30
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-04-27 19:34:45 +0200

    pal: windows: fix alert message encoding

--- src/pal/windows/p-alert.c
+++ src/pal/windows/p-alert.c
@@ -9,17 +9,17 @@ void
 pal_alert(const char *text, int error)
 {
     WCHAR msg[MAX_PATH];
+    MultiByteToWideChar(CP_UTF8, 0, text, -1, msg, MAX_PATH);
+
     if (error)
     {
-        swprintf(msg,
+        WCHAR error_msg[32];
+        swprintf(error_msg,
 #if !defined(_MSC_VER) || (_MSC_VER > 1400)
-                 MAX_PATH,
+                 lengthof(error_msg),
 #endif
-                 L"" FMT_AS L"\nerror %d", text, error);
-    }
-    else
-    {
-        MultiByteToWideChar(CP_UTF8, 0, text, -1, msg, MAX_PATH);
+                 L"\nerror %d", error);
+        wcsncat(msg, error_msg, MAX_PATH - wcslen(msg));
     }
 
     MessageBoxW(windows_wnd, msg, L"Lavender",
