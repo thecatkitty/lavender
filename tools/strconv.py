0992bdfd683cb9ef72ebd702a8ad167dc421197c
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-04-27 13:39:48 +0200

    tools: recursively create string array output directories

--- tools/strconv.py
+++ tools/strconv.py
@@ -1,15 +1,24 @@
+import os.path
 import re
 import subprocess
 
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser
+
+
+def output_file_type(path: str):
+    directory = os.path.dirname(path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+
+    return open(path, "w", encoding="utf-8")
+
 
 parser = ArgumentParser(
     prog="strconv",
     description="Lavender utility for converting Windows string resources to C",
     epilog="Copyright (c) Mateusz Karcz, 2025. Shared under the MIT License.")
 parser.add_argument("input", help="input Resource Compiler script")
-parser.add_argument("output", help="output C file",
-                    type=FileType("w", encoding="utf-8"))
+parser.add_argument("output", help="output C file", type=output_file_type)
 parser.add_argument("cc", help="C compiler")
 parser.add_argument("ccarg", nargs="*", help="C compiler arguments")
 parser.add_argument("--suffix", default="", help="array name suffix")
