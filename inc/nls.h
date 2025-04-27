bf328946f72f9a5f26b23bacde36187006ff0019
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-04-27 13:41:16 +0200

    nls: use IETF language names

--- inc/nls.h
+++ inc/nls.h
@@ -1,9 +1,9 @@
 #ifndef _NLS_H_
 #define _NLS_H_
 
-#define LCID_CSY 1029
-#define LCID_ENU 1033
-#define LCID_PLK 1045
+#define LCID_CS_CZ 1029
+#define LCID_EN_US 1033
+#define LCID_PL_PL 1045
 
 typedef struct
 {
