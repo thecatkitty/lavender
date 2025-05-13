2ea2c4b7b762e053f09e472463822e8353088f79
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-05-13 09:38:27 +0200

    gfx: cache pixel aspect ratio retrieved from VBE DC

--- src/gfx/vbe.c
+++ src/gfx/vbe.c
@@ -4,6 +4,8 @@
 #define _par(dx, dy, sx, sy)                                                   \
     (uint8_t)(64U * (uint32_t)dy * (uint32_t)sx / (uint32_t)dx / (uint32_t)sy)
 
+static uint16_t _ratio = 0;
+
 static bool
 _read_edid(edid_block *edid)
 {
@@ -37,6 +39,11 @@ _read_edid(edid_block *edid)
 uint16_t
 gfx_get_pixel_aspect(void)
 {
+    if (0 != _ratio)
+    {
+        return _ratio;
+    }
+
     gfx_dimensions screen;
     gfx_get_screen_dimensions(&screen);
 
@@ -48,8 +55,8 @@ gfx_get_pixel_aspect(void)
     edid_block edid;
     if (!_read_edid(&edid))
     {
-        return ratios[EDID_TIMING_ASPECT_4_3];
+        return _ratio = ratios[EDID_TIMING_ASPECT_4_3];
     }
 
-    return ratios[edid.standard_timing[0] >> EDID_TIMING_ASPECT];
+    return _ratio = ratios[edid.standard_timing[0] >> EDID_TIMING_ASPECT];
 }
