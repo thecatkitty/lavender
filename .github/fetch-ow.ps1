dc4d4ec72a082658690e16cf51cf2c1dac0a4637
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-05-23 00:30:19 +0200

    repo: build LARD for AXP on CI

---
+++ .github/fetch-ow.ps1
@@ -0,0 +1,6 @@
+curl.exe -kL "https://static.celones.pl/ci/lavender/resource_hacker.zip" -o ext/reshack.zip
+curl.exe -kL "https://static.celones.pl/ci/lavender/watcom-1.7-dec-alpha-crosscompiler.tar.xz" -o ext/watcom.txz
+7z e ext/reshack.zip -oext/reshack
+7z e ext/watcom.txz -oext
+7z x ext/watcom.tar -oC:\
+Remove-Item C:\WATCOM-axp\binnt\rc.exe
