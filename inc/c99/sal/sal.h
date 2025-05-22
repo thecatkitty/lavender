66121148bfea32aadd8eab06b5f56d35e6c0aa4d
Mateusz Karcz <mateusz.karcz@interia.eu>
2025-05-22 17:49:59 +0200

    ard: add headers missing from OpenWatcom

---
+++ inc/c99/sal/sal.h
@@ -0,0 +1,18 @@
+#ifndef _COMPAT_SAL_H_
+#define _COMPAT_SAL_H_
+
+#define _In_
+#define _In_opt_
+#define _In_z_
+
+#define _Out_
+#define _Out_z_cap_(x)
+#define _Out_writes_z_(x)
+
+#define _Inout_
+
+#define _Ret_maybenull_
+#define _Ret_z_
+#define _Success_(x)
+
+#endif
