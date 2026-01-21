999aece8545fe5d3824edc20741221888043a84f
Mateusz Karcz <mateusz.karcz@interia.eu>
2026-01-22 00:53:36 +0100

    repo: don't skip HTTPS verification on CI

--- .github/fetch-winsdk.ps1
+++ .github/fetch-winsdk.ps1
@@ -1,4 +1,4 @@
-curl.exe -kL https://download.microsoft.com/download/7/5/e/75ec7f04-4c8c-4f38-b582-966e76602643/5.2.3790.1830.15.PlatformSDK_Svr2003SP1_rtm.img -o ext/winsdk52.iso
+curl.exe -L https://download.microsoft.com/download/7/5/e/75ec7f04-4c8c-4f38-b582-966e76602643/5.2.3790.1830.15.PlatformSDK_Svr2003SP1_rtm.img -o ext/winsdk52.iso
 7z x ext/winsdk52.iso -oext/winsdk52
 
 "Installing Windows Server 2003 SP1 Platform SDK..."
