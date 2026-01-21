24895f2a04e25befa7fe16786870000c85236635
Mateusz Karcz <mateusz.karcz@interia.eu>
2026-01-22 00:53:36 +0100

    repo: don't skip HTTPS verification on CI

--- .github/fetch-ow.ps1
+++ .github/fetch-ow.ps1
@@ -1,5 +1,5 @@
-curl.exe -kL "https://static.celones.pl/ci/lavender/resource_hacker.zip" -o ext/reshack.zip
-curl.exe -kL "https://static.celones.pl/ci/lavender/watcom-1.7-dec-alpha-crosscompiler.tar.xz" -o ext/watcom.txz
+curl.exe -L "https://static.celones.pl/ci/lavender/resource_hacker.zip" -o ext/reshack.zip
+curl.exe -L "https://static.celones.pl/ci/lavender/watcom-1.7-dec-alpha-crosscompiler.tar.xz" -o ext/watcom.txz
 7z e ext/reshack.zip -oext/reshack
 7z e ext/watcom.txz -oext
 7z x ext/watcom.tar -oC:\
