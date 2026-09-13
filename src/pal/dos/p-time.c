e78d38dee3cb36d6d4efe36208f2ed19cd20c159
Mateusz Karcz <mateusz.karcz@interia.eu>
2026-09-13 18:51:04 +0200

    pal: dos: only use interrupt counter resolution
    Prevent rollover errors in runtime and logic errors in development.

--- src/pal/dos/p-time.c
+++ src/pal/dos/p-time.c
@@ -14,23 +14,14 @@ volatile uint32_t dos_counter;
 uint32_t ddcall
 pal_get_counter(void)
 {
-    uint16_t count = 0;
-
-    _disable();
-    outp(PIT_IO_COMMAND, 0x00);
-    count = inp(PIT_DATA(0));
-    count |= inp(PIT_DATA(0)) << 8;
-    _enable();
-
-    return (dos_counter << PIT_FREQ_POWER) |
-           (count & ((1 << PIT_FREQ_POWER) - 1));
+    return dos_counter;
 }
 
 uint32_t ddcall
 pal_get_ticks(unsigned ms)
 {
     uint64_t ticks = (uint64_t)ms * (PIT_INPUT_FREQ_F10 / 1000ULL);
-    ticks >>= 10;
+    ticks >>= 10 + PIT_FREQ_POWER;
     return (UINT32_MAX < ticks) ? UINT32_MAX : ticks;
 }
 
@@ -38,7 +29,7 @@ void ddcall
 pal_sleep(unsigned ms)
 {
     uint32_t until = pal_get_counter() + pal_get_ticks((uint32_t)ms);
-    while (pal_get_counter() < until)
+    while (pal_get_counter() <= until)
     {
         asm("hlt");
     }
