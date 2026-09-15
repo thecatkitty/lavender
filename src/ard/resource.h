90b60cfb2491e4c82b08cc7a0a0449a5ebb48051
Mateusz Karcz <mateusz.karcz@interia.eu>
2026-09-15 22:06:50 +0200

    ard: apply clang-format

--- src/ard/resource.h
+++ src/ard/resource.h
@@ -1,58 +1,58 @@
 #include <winnt.h>
 
 // Bitmaps
-#define IDB_BG           1
-#define IDB_BG16         2
+#define IDB_BG   1
+#define IDB_BG16 2
 
 // Icons
-#define IDI_APPICON      1
-#define IDI_INSTREDIST   2
-#define IDI_RUNDOS       3
-#define IDI_INSTIE       4
+#define IDI_APPICON    1
+#define IDI_INSTREDIST 2
+#define IDI_RUNDOS     3
+#define IDI_INSTIE     4
 
 // Strings
-#define IDS_INFORMATION  0x10
-#define IDS_INSTREDIST   0x11
-#define IDS_INSTREDISTD  0x12
-#define IDS_INSTREDISTS  0x13
-#define IDS_RUNDOS       0x14
-#define IDS_RUNDOSD      0x15
-#define IDS_INSTIE       0x16
-#define IDS_INSTIED      0x17
-#define IDS_INSTIEUP     0x18
-#define IDS_INSTIEUPD    0x19
-#define IDS_INSTREDISTO  0x1A
+#define IDS_INFORMATION 0x10
+#define IDS_INSTREDIST  0x11
+#define IDS_INSTREDISTD 0x12
+#define IDS_INSTREDISTS 0x13
+#define IDS_RUNDOS      0x14
+#define IDS_RUNDOSD     0x15
+#define IDS_INSTIE      0x16
+#define IDS_INSTIED     0x17
+#define IDS_INSTIEUP    0x18
+#define IDS_INSTIEUPD   0x19
+#define IDS_INSTREDISTO 0x1A
 
-#define IDS_DEFNAME      0x40
-#define IDS_DEFRUN       0x41
-#define IDS_DEFRUNDOS    0x42
-#define IDS_DEFCPU       0x48
+#define IDS_DEFNAME   0x40
+#define IDS_DEFRUN    0x41
+#define IDS_DEFRUNDOS 0x42
+#define IDS_DEFCPU    0x48
 
 #define IDS_DEFCOPYRIGHT 0x57
 
-#define IDS_OLDCPU       0x60
-#define IDS_OLDWIN       0x61
-#define IDS_OLDWINNT     0x62
-#define IDS_OLDSP        0x63
-#define IDS_OLDOSR       0x64
-#define IDS_NODLL        0x65
-#define IDS_REDIST       0x66
-#define IDS_DIERUNDOS    0x6F
+#define IDS_OLDCPU    0x60
+#define IDS_OLDWIN    0x61
+#define IDS_OLDWINNT  0x62
+#define IDS_OLDSP     0x63
+#define IDS_OLDOSR    0x64
+#define IDS_NODLL     0x65
+#define IDS_REDIST    0x66
+#define IDS_DIERUNDOS 0x6F
 
-#define IDS_CPUBASE      0x70
-#define IDS_CPUI486      0x71
-#define IDS_CPUP5        0x72
-#define IDS_CPUP6        0x73
-#define IDS_CPUSSE       0x74
-#define IDS_CPUSSE2      0x75
-#define IDS_CPUX64       0x76
+#define IDS_CPUBASE 0x70
+#define IDS_CPUI486 0x71
+#define IDS_CPUP5   0x72
+#define IDS_CPUP6   0x73
+#define IDS_CPUSSE  0x74
+#define IDS_CPUSSE2 0x75
+#define IDS_CPUX64  0x76
 
-#define IDS_ECONFIG      0x101
-#define IDS_EEXECNORES   0x102
-#define IDS_EEXECBADF    0x103
-#define IDS_EEXECNOF     0x104
-#define IDS_EBADDEPS     0x105
-#define IDS_EBADHWND     0x106
-#define IDS_EINSTNOF     0x107
-#define IDS_EINSTBAD     0x108
-#define IDS_EINSTAGAIN   0x109
+#define IDS_ECONFIG    0x101
+#define IDS_EEXECNORES 0x102
+#define IDS_EEXECBADF  0x103
+#define IDS_EEXECNOF   0x104
+#define IDS_EBADDEPS   0x105
+#define IDS_EBADHWND   0x106
+#define IDS_EINSTNOF   0x107
+#define IDS_EINSTBAD   0x108
+#define IDS_EINSTAGAIN 0x109
